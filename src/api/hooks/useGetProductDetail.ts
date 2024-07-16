44b798ec1c59990f0ae1dacf6bfc3af9b2629ac1
박건규 <geongyu09@gmail.com>
2024-07-16 17:29:02 +0900

    feat: 디테일 페이지 api 구현

--- src/api/hooks/useGetProductDetail.ts
+++ src/api/hooks/useGetProductDetail.ts
@@ -4,41 +4,49 @@ import type { GoodsData } from '@/types';
 
 import { fetchInstance } from '../instance';
 
-type ProductDetaiResponseData = GoodsData & {
-  isAccessableProductPage: boolean;
-  review: {
-    averageRating: number;
-    totalReviewCount: number;
-  };
-  productDescription: {
-    images: string[];
-  };
-  productDetailInfo: {
-    announcements: {
-      displayOrder: number;
-      name: string;
-      value: string;
-    }[];
-    terms: {
-      displayOrder: number;
-      title: string;
-      description: string;
-    }[];
+type ProductDetaiResponseData = {
+  detail: GoodsData & {
+    isAccessableProductPage: boolean;
+    review: {
+      averageRating: number;
+      totalReviewCount: number;
+    };
+    productDescription: {
+      images: string[];
+    };
+    productDetailInfo: {
+      announcements: {
+        displayOrder: number;
+        name: string;
+        value: string;
+      }[];
+      terms: {
+        displayOrder: number;
+        title: string;
+        description: string;
+      }[];
+    };
   };
 };
 
-const getProductDetailPath = (productId: number) => `/v1/products/${productId}/detail`;
-const productDetailQueryKey = (productId: number) => [getProductDetailPath(productId)];
+const getProductDetailPath = (productId: string) => `/v1/products/${productId}/detail`;
+const productDetailQueryKey = (productId: string) => [getProductDetailPath(productId)];
 
-export const getProductDetail = async (productId: number) => {
+export const getProductDetail = async (productId: string) => {
   const response = await fetchInstance.get<ProductDetaiResponseData>(
     getProductDetailPath(productId),
   );
   return response.data;
 };
 
-export const useGetProductDetail = (productId: number) =>
-  useQuery({
-    queryKey: productDetailQueryKey(productId),
-    queryFn: () => getProductDetail(productId),
+export const useGetProductDetail = (productId: string | undefined) => {
+  if (!productId) {
+    productId = '';
+  }
+
+  return useQuery({
+    queryKey: productDetailQueryKey(productId as string),
+    queryFn: () => getProductDetail(productId as string),
+    enabled: !!productId,
   });
+};
