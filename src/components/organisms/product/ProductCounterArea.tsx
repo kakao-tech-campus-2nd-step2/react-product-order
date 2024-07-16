0434fb5c4d0203047638e4b31090be41a1772725
cla6shade <cla6shade@gmail.com>
2024-07-16 14:29:45 +0900

    feat: 상품 상세정보 페이지의 카운터 구현

---
+++ src/components/organisms/product/ProductCounterArea.tsx
@@ -0,0 +1,40 @@
+import Container from '@components/atoms/container/Container';
+import {
+  Box, Button, HStack, Input, Text, useNumberInput,
+} from '@chakra-ui/react';
+import { defaultBorderColor } from '@styles/colors';
+import { ProductDetailData } from '@/dto';
+
+interface ProductCounterAreaProps {
+  productDetails: ProductDetailData;
+}
+
+function ProductCounterArea({ productDetails }: ProductCounterAreaProps) {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } = useNumberInput({
+    step: 1,
+    defaultValue: 1,
+    min: 1,
+    max: 100,
+  });
+
+  return (
+    <Container padding="30px 12px 30px 30px">
+      <Box
+        w="100%"
+        padding="12px 14px 16px"
+        border="rgb(237, 237, 237) 1px solid"
+        display="flex"
+        flexDirection="column"
+      >
+        <Text fontWeight="bold">{productDetails.name}</Text>
+        <HStack w="100%" paddingTop="10px">
+          <Button {...getDecrementButtonProps()}>-</Button>
+          <Input {...getInputProps()} borderColor={defaultBorderColor} />
+          <Button {...getIncrementButtonProps()}>+</Button>
+        </HStack>
+      </Box>
+    </Container>
+  );
+}
+
+export default ProductCounterArea;
