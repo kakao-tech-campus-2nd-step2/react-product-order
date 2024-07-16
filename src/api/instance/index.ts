6eaf81501a93220d054e81486e7e05d7cc86f7cd
박건규 <geongyu09@gmail.com>
2024-07-16 17:27:34 +0900

    fix: baseURL 변경

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -17,7 +17,7 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
 };
 
 export const fetchInstance = initInstance({
-  baseURL: 'https://kakao-tech-campus-mock-server.vercel.app/api',
+  baseURL: 'https://react-gift-mock-api-geongyu09.vercel.app/api',
 });
 
 export const queryClient = new QueryClient({
