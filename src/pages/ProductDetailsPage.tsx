28abc0a67607c48a363e8d81e02bdb1c700650ac
cla6shade <cla6shade@gmail.com>
2024-07-16 13:58:35 +0900

    feat: 제품 상세정보 표시

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,34 +1,25 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { QueryKeys } from '@constants/QueryKeys';
-import { fetchProductDetail } from '@utils/query';
-import { useEffect } from 'react';
-import Paths from '@constants/Paths';
-import axios from 'axios';
-import { StatusCodes } from 'http-status-codes';
+import { useParams } from 'react-router-dom';
+import { Suspense } from 'react';
 import Page from '@components/templates/Page';
 import Container from '@components/atoms/container/Container';
 import { MAX_CONTENT_WIDTH } from '@styles/size';
+import ProductDetailDisplaySection from '@components/organisms/product/ProductDetailDisplaySection';
+import { LoadingSpinnerFullWidth } from '@components/atoms/LoadingSpinner';
+import ErrorBoundary from '@components/atoms/boundary/ErrorBoundary';
 
 function ProductDetailsPage() {
   const { productId } = useParams<string>();
-  const { data, status, error } = useSuspenseQuery({
-    queryKey: [QueryKeys.PRODUCT_DETAILS, productId],
-    queryFn: () => fetchProductDetail({ productId: productId as string }),
-  });
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!productId
-      || (error && axios.isAxiosError(error) && error.response?.status === StatusCodes.NOT_FOUND)) {
-      navigate(Paths.MAIN_PAGE);
-    }
-  }, [error, navigate, productId]);
 
   return (
     <Page>
       <Container elementSize="full-width" justifyContent="center">
-        <Container elementSize="full-width" maxWidth={MAX_CONTENT_WIDTH} />
+        <Container elementSize="full-width" maxWidth={MAX_CONTENT_WIDTH}>
+          <ErrorBoundary>
+            <Suspense fallback={<LoadingSpinnerFullWidth />}>
+              <ProductDetailDisplaySection productId={productId} />
+            </Suspense>
+          </ErrorBoundary>
+        </Container>
       </Container>
     </Page>
   );
