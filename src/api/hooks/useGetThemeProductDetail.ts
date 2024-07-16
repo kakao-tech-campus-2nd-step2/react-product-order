15c1f70ebbced15d6f0820b67a1814e120ca85b6
Jisu Lee <dlwltn0430@naver.com>
2024-07-16 15:20:26 +0900

    feat: 상품 상세정보 가져오는 로직 구현

---
+++ src/api/hooks/useGetThemeProductDetail.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+
+import type { ProductDetailData } from '@/types';
+
+import { fetchInstance } from '../instance';
+
+export type ProductDetailResponseData = {
+  productDetail: ProductDetailData[];
+};
+
+const getThemesProductsDetailPath = (productId: string) => `/v1/products/${productId}/detail`;
+
+export const getProductDetail = async (productId: string) => {
+  const response = await fetchInstance.get<ProductDetailResponseData>(
+    getThemesProductsDetailPath(productId),
+  );
+  return response.data;
+};
+
+export const useGetThemeProductDetail = (productId: string) =>
+  useQuery({
+    queryKey: [getThemesProductsDetailPath(productId)],
+    queryFn: () => getProductDetail(productId),
+  });
