8998621f8f558f802a481129791968af73e57bb2
dandamdandam <glue0440@gmail.com>
2024-07-17 07:37:38 +0900

    feat: go backward if login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ export default function Login() {
         event.preventDefault();
         authSessionStorage.set(username);
         setIsAuthenticated(true);
-        navigate('/');
+        navigate(-1);
     };
 
     return (
