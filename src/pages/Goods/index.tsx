8e8d64f05be33224c800f71acb2281495e080bbe
KimKyuHoi <k546kh@gmail.com>
2024-07-16 15:47:30 +0900

    Feat: Goods Detail 페이지 Suspense Boundary 설정

--- src/pages/Goods/index.tsx
+++ src/pages/Goods/index.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import type { GoodsDetailRequestParams } from '@/api/hooks/useGetGoodsDetail';
 import { SplitLayout } from '@/components/common/layouts/SplitLayout';
+import { LoadingView } from '@/components/common/LoadingView/LoadingView';
+import { SuspenseBoundary } from '@/components/common/SuspenseBoundary';
 import { GoodsDetail } from '@/components/features/Goods';
 import { OptionSection } from '@/components/features/Goods/OptionSection';
 
@@ -9,8 +12,12 @@ export const GoodsDetailPage = () => {
   const { productId = '' } = useParams<GoodsDetailRequestParams>();
 
   return (
-    <SplitLayout sidebar={<OptionSection productId={productId} />}>
-      <GoodsDetail productId={productId} />
-    </SplitLayout>
+    <React.Fragment>
+      <SuspenseBoundary pendingFallback={<LoadingView />} rejectedFallback={<div>에러 페이지</div>}>
+        <SplitLayout sidebar={<OptionSection productId={productId} />}>
+          <GoodsDetail productId={productId} />
+        </SplitLayout>
+      </SuspenseBoundary>
+    </React.Fragment>
   );
 };
