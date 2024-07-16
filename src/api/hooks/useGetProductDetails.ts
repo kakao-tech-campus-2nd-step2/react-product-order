676caf9a464a1e31caea52b8f4065ba8905894ab
KimJi-An <j0012a@naver.com>
2024-07-16 18:56:06 +0900

    feat: 상품 클릭 시 상품 상세 페이지로 연결 기능 추가

---
+++ src/api/hooks/useGetProductDetails.ts
@@ -0,0 +1,32 @@
+import type { UseQueryResult } from "@tanstack/react-query";
+import { useQuery} from "@tanstack/react-query";
+
+import type { GoodsData } from "@/types";
+
+import { fetchInstance } from "../instance";
+
+type RequestParams = {
+  productId: string;
+};
+
+type ProductDetailsResponseData = {
+  detail: GoodsData;
+};
+
+const getProductDetailsPath = ({ productId }: RequestParams) => {
+  return `/v1/products/${productId}/detail`;
+};
+
+export const getProductDetails = async (params: RequestParams) => {
+  const response = await fetchInstance.get<ProductDetailsResponseData>(
+    getProductDetailsPath(params),
+  );
+  return response.data.detail;
+};
+
+export const useGetProductDetails = (productId: string): UseQueryResult<GoodsData> => 
+  useQuery<GoodsData>({
+    queryKey: ['detail', productId],
+    queryFn: () => getProductDetails({ productId }),
+    enabled: !!productId,
+  });
--- src/routes/path.ts
+++ src/routes/path.ts
@@ -2,6 +2,7 @@ export const RouterPath = {
   root: '/',
   home: '/',
   theme: '/theme/:themeKey',
+  products: '/products/:productId',
   myAccount: '/my-account',
   login: '/login',
   notFound: '*',
@@ -9,6 +10,7 @@ export const RouterPath = {
 
 export const getDynamicPath = {
   theme: (themeKey: string) => RouterPath.theme.replace(':themeKey', themeKey),
+  product: (productId: number) => RouterPath.products.replace(':productId', productId.toString()),
   login: (redirect?: string) => {
     const currentRedirect = redirect ?? window.location.href;
     return `${RouterPath.login}?redirect=${encodeURIComponent(currentRedirect)}`;
