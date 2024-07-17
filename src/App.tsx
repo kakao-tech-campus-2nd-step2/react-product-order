2af95dd8af6f0df5b2a1ae1986ee49d023edace4
minji2219 <200146@jnu.ac.kr>
2024-07-18 00:41:31 +0900

    Feat: order페이지에서도 buyInfo context 사용할 수 있게 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 
 import { queryClient } from './api/instance';
 import { AuthProvider } from './provider/Auth';
+import { BuyInfoProvider } from './provider/BuyInfo';
 import { Routes } from './routes';
 
 const App = () => {
@@ -10,7 +11,9 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <AuthProvider>
-          <Routes />
+          <BuyInfoProvider>
+            <Routes />
+          </BuyInfoProvider>
         </AuthProvider>
       </ChakraProvider>
     </QueryClientProvider>
