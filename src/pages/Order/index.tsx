59ad99064a256a668781a30baa52a1a6fc7036c8
dandamdandam <glue0440@gmail.com>
2024-07-17 14:24:54 +0900

    feat: design order detail section

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,14 +1,43 @@
+import { css } from '@emotion/css';
+
 import Header from '@/components/features/Header';
 
 import InputMsg from './InputMsg';
+import OrderDetail from './OrderDetail';
 
 export default () => {
     return (
         <div>
             <Header />
             <form>
-                <InputMsg />
+                <div className={gridStyle}>
+                    <div>
+                        <InputMsg />
+                        <div className={dividerStyle} />
+                        <OrderDetail
+                            imageURL={
+                                'https://st.kakaocdn.net/product/gift/product/20230823153529_37aa37bcef074955ab6548f7fc799c18.jpg'
+                            }
+                            productName={'product'}
+                            brandName={'brand'}
+                            quantity={1}
+                        />
+                    </div>
+                    <div></div>
+                </div>
             </form>
         </div>
     );
 };
+
+const gridStyle = css`
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+`;
+
+const dividerStyle = css`
+    border-bottom: 1px solid #eee;
+    border-top: 1px solid #eee;
+    height: 10px;
+    background-color: #f9f9f9;
+`;
