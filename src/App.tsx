b981cd757cb1109ae1ace2962fbb9e7df1c111ea
userjmmm <a@as-MacBook-Pro.local>
2024-07-17 14:40:33 +0900

    feat: 전체 App에 적용되도록 ChakraProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './api/instance';
 import { AuthProvider } from './provider/Auth';
 import { Routes } from './routes';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 };
 
