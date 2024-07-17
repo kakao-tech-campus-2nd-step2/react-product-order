be33c69edfa1613a9eed261656733f5feb125198
dlawlghks <ikuwlghks@naver.com>
2024-07-17 20:16:06 +0900

    feat: 상품 상세 정보 조회 및 캐시 기능 추가

---
+++ src/api/hooks/useFetchProductHistory.ts
@@ -0,0 +1,28 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { fetchInstance } from '@/api/instance';
+import type { GoodsData } from '@/types';
+
+export type GoodsDetailRequestParams = {
+  productId: string;
+};
+
+export type GoodsDetailResponseData = {
+  detail: GoodsData;
+};
+
+const getGoodsDetailPath = (goodsId: string) => `/v1/products/${goodsId}/detail`;
+
+export const getGoodsDetail = async (params: GoodsDetailRequestParams) => {
+  const response = await fetchInstance.get<GoodsDetailResponseData>(
+    getGoodsDetailPath(params.productId),
+  );
+  return response.data.detail;
+};
+
+export const useFetchProductHistory = (productId: string) => {
+  return useSuspenseQuery({
+    queryKey: [getGoodsDetailPath(productId)],
+    queryFn: () => getGoodsDetail({ productId }),
+  });
+};
