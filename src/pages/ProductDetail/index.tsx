f3e8542c1ff282346153d9dc382bf5d2bbdaad2e
박건규 <geongyu09@gmail.com>
2024-07-17 12:40:21 +0900

    refactor: 받아온 파라미터값이 undefined인 경우를 대비해 따로 컴포넌트 분리하기
    usePrams로 받아온 값은 query문에 들어가야하는 파라미터이지만, 이 undefined
    일 수 있으므로, 둘의 책임을 컴포넌트로 따로 빼서 useParams로 받아온 값이 undefined이지 않을 경우에만 렌더링을 하도록 수정

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,75 +1,21 @@
-import { Box, Button, Image, Input, Stack, Text } from '@chakra-ui/react';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { useGetProductDetail } from '@/api/hooks/useGetProductDetail';
 import { Container } from '@/components/common/layouts/Container';
+import ProductDetailSection from '@/components/features/Detail/ProductDetailSection';
 
 const ProductDetialPage = () => {
+  const navigate = useNavigate();
+
   const { productId } = useParams();
-  const { data, isLoading, error } = useGetProductDetail(productId);
-  const [count, setCount] = useState(1);
 
-  if (isLoading || !data) {
-    return <div>loading...</div>;
+  if (!productId) {
+    navigate('/');
+    return <></>;
   }
-  if (error) {
-    return <div>error...</div>;
-  }
-
-  const {
-    imageURL,
-    name,
-    price: { basicPrice },
-  } = data.detail;
 
   return (
     <Container>
-      <Stack direction="row">
-        {data && (
-          <>
-            <Box width={'50%'}>
-              <Image src={imageURL} alt={name} />
-            </Box>
-            <Box>
-              <div>{name}</div>
-              <div>{basicPrice}</div>
-            </Box>
-
-            <Stack direction="column" justifyContent="space-between">
-              <Box border="1px solid" height="fit-content">
-                <Text fontWeight="bold">{name}</Text>
-                <Stack direction="row">
-                  <Button width={10} onClick={() => setCount((prev) => prev + 1)}>
-                    +
-                  </Button>
-                  <Input
-                    type="number"
-                    value={count}
-                    onChange={(e) => setCount(Number(e.target.value))}
-                  />
-                  <Button
-                    onClick={() => {
-                      if (count > 1) {
-                        setCount((prev) => prev - 1);
-                      }
-                    }}
-                  >
-                    -
-                  </Button>
-                </Stack>
-              </Box>
-
-              <Stack>
-                <Text width="100%" padding="1rem" backgroundColor="whitesmoke">
-                  총 가격: {basicPrice * count}
-                </Text>
-                <Button flex="grow">선물하기</Button>
-              </Stack>
-            </Stack>
-          </>
-        )}
-      </Stack>
+      <ProductDetailSection productId={productId} />
     </Container>
   );
 };
