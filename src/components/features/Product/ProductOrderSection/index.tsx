f7e036b38dfb18cfde33d4f4afe209d3ea3828bf
KimJi-An <j0012a@naver.com>
2024-07-18 02:10:51 +0900

    style: 코드 포맷팅 적용

--- src/components/features/Product/ProductOrderSection/index.tsx
+++ src/components/features/Product/ProductOrderSection/index.tsx
@@ -1,4 +1,14 @@
-import { Button, Container, Flex, NumberDecrementStepper, NumberIncrementStepper, NumberInput,NumberInputField,NumberInputStepper,Text } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Flex,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Text,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -18,50 +28,45 @@ export const ProductOrderSection = (data: GoodsData) => {
 
     if (!authToken) {
       navigate(getDynamicPath.login());
-    }
-    else {
-      navigate(getDynamicPath.order(data.id), { state: { quantity }});
+    } else {
+      navigate(getDynamicPath.order(data.id), { state: { quantity } });
     }
   };
 
   return (
     <Container
-      as='aside'
-      position='sticky'
-      top='54px'
-      w='100%'
-      maxW='360px'
-      h='calc(-54px + 100vh)'
+      as="aside"
+      position="sticky"
+      top="54px"
+      w="100%"
+      maxW="360px"
+      h="calc(-54px + 100vh)"
     >
       <Flex
-        w='100%'
-        padding='30px 12px 30px 30px'
-        h='100%'
-        flexDirection='column'
-        justifyContent='space-between'
+        w="100%"
+        padding="30px 12px 30px 30px"
+        h="100%"
+        flexDirection="column"
+        justifyContent="space-between"
       >
         <Container
-          w='100%'
-          padding='12px 14px 16px'
-          border='1px solid rgb(237, 237, 237)'
-          borderRadius='2px'
+          w="100%"
+          padding="12px 14px 16px"
+          border="1px solid rgb(237, 237, 237)"
+          borderRadius="2px"
         >
           <Text
             fontWeight={700}
-            lineHeight='22px'
-            color='rgb(17, 17, 17)'
-            overflowWrap='break-word'
-            wordBreak='break-all'
+            lineHeight="22px"
+            color="rgb(17, 17, 17)"
+            overflowWrap="break-word"
+            wordBreak="break-all"
           >
             {data.name}
           </Text>
-          <Flex
-            justifyContent='center'
-            paddingTop='8px'
-            gap='8px'
-          >
+          <Flex justifyContent="center" paddingTop="8px" gap="8px">
             <NumberInput
-              w='100%'
+              w="100%"
               defaultValue={1}
               min={1}
               max={100}
@@ -75,37 +80,32 @@ export const ProductOrderSection = (data: GoodsData) => {
             </NumberInput>
           </Flex>
         </Container>
-        <Container padding='12px 0px 0px'>
+        <Container padding="12px 0px 0px">
           <Flex
-            marginBottom='20px'
-            padding='18px 20px'
-            borderRadius='4px'
-            backgroundColor='rgb(245, 245, 245)'
-            justifyContent='space-between'
-            fontSize='14px'
+            marginBottom="20px"
+            padding="18px 20px"
+            borderRadius="4px"
+            backgroundColor="rgb(245, 245, 245)"
+            justifyContent="space-between"
+            fontSize="14px"
             fontWeight={700}
-            lineHeight='14px'
-            color='rgb(17, 17, 17)'
+            lineHeight="14px"
+            color="rgb(17, 17, 17)"
           >
             총 결제 금액
-            <Text
-              fontSize='20px'
-              fontWeight={700}
-              lineHeight='14px'
-              color='rgb(17, 17, 17)'
-            >
+            <Text fontSize="20px" fontWeight={700} lineHeight="14px" color="rgb(17, 17, 17)">
               {data.price.sellingPrice * quantity}원
             </Text>
           </Flex>
           <Button
-            w='100%'
-            boxSizing='border-box'
-            borderRadius='4px'
-            cursor='pointer'
-            h='60px'
-            fontSize='16px'
-            color='rgb(255, 255, 255)'
-            backgroundColor='rgb(17, 17, 17)'
+            w="100%"
+            boxSizing="border-box"
+            borderRadius="4px"
+            cursor="pointer"
+            h="60px"
+            fontSize="16px"
+            color="rgb(255, 255, 255)"
+            backgroundColor="rgb(17, 17, 17)"
             onClick={handleGiftClick}
           >
             나에게 선물하기
