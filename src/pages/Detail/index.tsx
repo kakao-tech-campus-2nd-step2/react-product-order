913de69e5a3adf4d0cb0607088a3c65fe08fba94
minji2219 <200146@jnu.ac.kr>
2024-07-17 16:24:14 +0900

    Feat: 디테일 페이지

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,3 +1,55 @@
-export const DetailPage = ()=>{
-  return (<div>디테일페이지</div>)
-}
+import styled from '@emotion/styled';
+import { useParams } from 'react-router-dom';
+
+import { useGetDetail } from '@/api/hooks/useGetProductDetail';
+import { Image } from '@/components/common/Image';
+import { Container } from '@/components/common/layouts/Container';
+import { Spinner } from '@/components/common/Spinner';
+import { ProductDescript } from '@/components/features/Detail/ProductDescript';
+import { QuantitySetting } from '@/components/features/Detail/QuantitySetting';
+import { TotalPrice } from '@/components/features/Detail/TotalPrice';
+import { BuyInfoProvider } from '@/provider/BuyInfo';
+import { breakpoints } from '@/styles/variants';
+
+export const DetailPage = () => {
+  const params = useParams();
+
+  const { data, error, isLoading } = useGetDetail(params.productKey!);
+
+  if (isLoading)
+    return (
+      <View>
+        <Spinner />
+      </View>
+    );
+
+  if (error) return <div>에러입니다.</div>;
+
+  if (!data) return <></>;
+  return (
+    <BuyInfoProvider>
+      <Container flexDirection="row" maxWidth={breakpoints.lg} style={{ marginTop: 30 }}>
+        <Image src={data.imageURL} width={450} />
+        <ProductDescript name={data.name} price={data.price.sellingPrice} />
+        <Wrapper>
+          <QuantitySetting name={data.name} />
+          <TotalPrice />
+        </Wrapper>
+      </Container>
+    </BuyInfoProvider>
+  );
+};
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 80vh;
+  margin-bottom: 20px;
+`;
+const View = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 80vh;
+  margin-top: 30px;
+`;
