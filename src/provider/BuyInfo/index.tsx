d33f4e2dd9fad9557ef014e0a255467a99b16059
minji2219 <200146@jnu.ac.kr>
2024-07-17 15:47:14 +0900

    Feat: BuyInfo context default값 추가

--- src/provider/BuyInfo/index.tsx
+++ src/provider/BuyInfo/index.tsx
@@ -1,15 +1,19 @@
 import { type ReactNode, useContext, useState } from 'react';
 import { createContext } from 'react';
 
-export const BuyInfoContext = createContext<Context | undefined>(undefined);
-
-type Context = {
+export type Context = {
   price: number;
   setPrice: (price: number) => void;
   quantity: number;
   setQuantity: (quantity: number) => void;
 };
-
+const defaultBuyInfo = {
+  price: 0,
+  setPrice: () => {},
+  quantity: 1,
+  setQuantity: () => {},
+};
+export const BuyInfoContext = createContext<Context>(defaultBuyInfo);
 export const BuyInfoProvider = ({ children }: { children: ReactNode }) => {
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(1);
