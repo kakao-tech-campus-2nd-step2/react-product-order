0939fc204b921bcf089610c1132324bbdb315236
JaeAnHan <jaean1999@naver.com>
2024-07-18 15:00:48 +0900

    feat: Order 페이지 추가 및 라우팅 구현

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from '@/components/features/Layout';
 import { HomePage } from '@/pages/Home';
 import { LoginPage } from '@/pages/Login';
 import { MyAccountPage } from '@/pages/MyAccount';
+import { OrderPage } from '@/pages/Order';
 import { ThemePage } from '@/pages/Theme';
 import { ProductRoute } from '@/routes/components/ProductRoute';
 
@@ -28,12 +29,16 @@ const router = createBrowserRouter([
         element: <ProductRoute />,
       },
       {
-        path: RouterPath.myAccount,
+        path: '',
         element: <PrivateRoute />,
         children: [
           {
             path: RouterPath.myAccount,
             element: <MyAccountPage />,
+          },
+          {
+            path: RouterPath.order,
+            element: <OrderPage />,
           },
         ],
       },
