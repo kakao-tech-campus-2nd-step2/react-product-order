8c3369ce0c3fb46f9606c94d7b093faa462a83e1
Dobbymin <gmin0701@knu.ac.kr>
2024-07-18 16:25:41 +0900

    feat(order): 상품 구매 페이지 ui 구현

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,18 +1,17 @@
-import Main from '../../../.storybook/main.ts';
-import * as S from './styles.ts';
-import {
-  AsideTitle,
-  giftDetailBox,
-  giftImgBox,
-  giftListSection,
-} from './styles.ts';
+import * as S from './styles';
 import { Container } from '@/components/common/layouts/Container';
 import { AsideBox } from '@/components/features/Product/ProductBuySection/styles.ts';
+import { MainBox } from '@/components/features/Product/ProductInfoSection/styles.ts';
 import {
-  DividerBar,
-  MainBox,
-} from '@/components/features/Product/ProductInfoSection/styles.ts';
-import { Box, Divider, Image, Text, Textarea } from '@chakra-ui/react';
+  Box,
+  Checkbox,
+  Divider,
+  Image,
+  Input,
+  Select,
+  Text,
+  Textarea,
+} from '@chakra-ui/react';
 
 const OrderPage = () => {
   return (
@@ -121,11 +120,71 @@ const OrderPage = () => {
                 borderRight='1px solid rgb(237, 237, 237)'
                 padding='16px'
               >
-                <S.AsideTitle>결제 정보</S.AsideTitle>
+                <S.AsideTitleBox>
+                  <Text
+                    fontSize='18px'
+                    lineHeight='21px'
+                    color='rgb(34, 34, 34)'
+                    fontWeight='bold'
+                  >
+                    결제 정보
+                  </Text>
+                </S.AsideTitleBox>
+                <Divider borderWidth='0px 0px 1px' color='rgb(237, 237, 237)' />
+                <Box width='100%' padding='16px'>
+                  <Checkbox>
+                    <Text
+                      fontSize='15px'
+                      lineHeight='24px'
+                      fontWeight='700'
+                      color='rgb(0, 0, 0)'
+                    >
+                      현금영수증 신청
+                    </Text>
+                  </Checkbox>
+                  <Box
+                    width='100%'
+                    backgroundColor='inherit'
+                    height='16px'
+                  ></Box>
+                  <Select width='100%'>
+                    <S.OptionItems value='PERSONAL'>개인소득공제</S.OptionItems>
+                    <S.OptionItems value='BUSINESS'>사업자증빙용</S.OptionItems>
+                  </Select>
+                  <Box
+                    height='8px'
+                    width='100%'
+                    backgroundColor='inherit'
+                  ></Box>
+                  <Input
+                    width='100%'
+                    name='cashReceiptNumber'
+                    placeholder='(-없이) 숫자만 입력해주세요.'
+                  />
+                </Box>
                 <Divider />
-
+                <S.PriceBox>
+                  <Text
+                    fontSize='15px'
+                    lineHeight='24px'
+                    fontWeight='700'
+                    color='rgb(0, 0, 0)'
+                  >
+                    최종 결제금액
+                  </Text>
+                  <Text
+                    fontSize='18px'
+                    lineHeight='21px'
+                    fontWeight='700'
+                    color='rgb(34, 34, 34)'
+                    boxSizing='border-box'
+                  >
+                    11500원
+                  </Text>
+                </S.PriceBox>
                 <Divider />
-                <Divider />
+                <Box width='100%' height='32px' backgroundColor='inherit'></Box>
+                <S.SubmitButton>11500원 결제하기</S.SubmitButton>
               </Box>
             </AsideBox>
           </Box>
