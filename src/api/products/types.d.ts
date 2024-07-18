c58ba39dcbe1fd417c8a2c6bf11623e99c23fef0
JaeAnHan <jaean1999@naver.com>
2024-07-19 02:44:55 +0900

    feat: ProductOrderRequestBody 타입 추가

--- src/api/products/types.d.ts
+++ src/api/products/types.d.ts
@@ -61,5 +61,18 @@ declare namespace Products {
     cnt: string;
   }
 
+  interface ProductOrderRequestBody {
+    productId: number;
+    productOptionId: number;
+    productQuantity: number;
+    messageCardTemplateId: number;
+    messageCardTextMessage: string;
+    senderId: number;
+    receiverId: number;
+    hasCashReceipt: boolean;
+    cashReceiptType?: 'PERSONAL' | 'BUSINESS';
+    cashReceiptNumber?: string;
+  }
+
   // }
 }
