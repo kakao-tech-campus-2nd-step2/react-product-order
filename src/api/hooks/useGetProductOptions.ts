4ac6e30edacc5942e75623e29a3dfc9339c1071d
KimJi-An <j0012a@naver.com>
2024-07-18 17:07:37 +0900

    feat: 상품의 개수가 giftOrderLimit을 초과한 경우 선택 불가

---
+++ src/api/hooks/useGetProductOptions.ts
@@ -0,0 +1,33 @@
+import type { UseQueryResult } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
+
+import type { OptionData } from "@/types";
+
+import { fetchInstance } from "../instance";
+
+type RequestParams = {
+  productId: string;
+};
+
+type ProductOptionsResponseData = {
+  options: OptionData;
+};
+
+const getProductOptionsPath = ({ productId }: RequestParams) => {
+  return `v1/products/${productId}/options`;
+};
+
+export const getProductOptions = async (params: RequestParams) => {
+  const response = await fetchInstance.get<ProductOptionsResponseData>(
+    getProductOptionsPath(params),
+  );
+  return response.data.options;
+};
+
+export const useGetProductOptions = (productId: string): UseQueryResult<OptionData> => {
+  return useQuery<OptionData>({
+    queryKey: ['options', productId],
+    queryFn: () => getProductOptions({ productId }),
+    enabled: !!productId,
+  });
+};
