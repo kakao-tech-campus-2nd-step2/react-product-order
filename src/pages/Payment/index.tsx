3f22cbe08bb917c02ef03655da31aba5af3065ad
mingkyeongg <ming0820@naver.com>
2024-07-19 02:25:45 +0900

    feat: form validation 구현

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -41,6 +41,7 @@ const PaymentPage: React.FC<PaymentPageProps> = ({ data, loading, errorMessage }
 		receiptNumber,
 		setReceiptNumber,
 		handleReceiptTypeChange,
+		handleSubmit,
 	} = useForm();
 	
 	
@@ -63,72 +64,74 @@ const PaymentPage: React.FC<PaymentPageProps> = ({ data, loading, errorMessage }
 	return (
 		<>
 		{data && <Box maxW="1200px" mx="auto" p={4}>
-		<HStack spacing={8} align="flex-start">
-			<VStack spacing={8} align="stretch" flex={2}>
-			<Box>
-				<Text fontSize="2xl" fontWeight="bold">나에게 주는 선물</Text>
-				<Textarea
-				mt={4}
-				placeholder="선물과 함께 보낼 메시지를 적어보세요"
-				value={message}
-				onChange={(e) => setMessage(e.target.value)}
-				bg="gray.50"
-				/>
-			</Box>
-			<Box>
-				<Text fontSize="2xl" fontWeight="bold" mb={4}>선물내역</Text>
-				<Box p={4} borderWidth="1px" borderRadius="md">
-				<HStack>
-					<Image
-					boxSize="100px"
-					objectFit="cover"
-					src="https://st.kakaocdn.net/product/gift/product/20240703140657_19263fd5455146b0a308a4e0d6bacc6a.png"
-					alt="선물 이미지"
-					/>
-					<VStack align="start" spacing={1}>
-					<Text fontWeight="bold">{data?.options.productName}</Text>
-					<Text> X 1개</Text>
+			<form onSubmit={handleSubmit}>
+				<HStack spacing={8} align="flex-start">
+					<VStack spacing={8} align="stretch" flex={2}>
+					<Box>
+						<Text fontSize="2xl" fontWeight="bold">나에게 주는 선물</Text>
+						<Textarea
+						mt={4}
+						placeholder="선물과 함께 보낼 메시지를 적어보세요"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						bg="gray.50"
+						/>
+					</Box>
+					<Box>
+						<Text fontSize="2xl" fontWeight="bold" mb={4}>선물내역</Text>
+						<Box p={4} borderWidth="1px" borderRadius="md">
+						<HStack>
+							<Image
+							boxSize="100px"
+							objectFit="cover"
+							src="https://st.kakaocdn.net/product/gift/product/20240703140657_19263fd5455146b0a308a4e0d6bacc6a.png"
+							alt="선물 이미지"
+							/>
+							<VStack align="start" spacing={1}>
+							<Text fontWeight="bold">{data?.options.productName}</Text>
+							<Text> X 1개</Text>
+							</VStack>
+						</HStack>
+						</Box>
+					</Box>
+					</VStack>
+					<VStack spacing={8} align="stretch" flex={1}>
+					<Text fontSize="2xl" fontWeight="bold" mb={4}>결제 정보</Text>
+					<Stack spacing={4}>
+						<Checkbox
+						isChecked={receiptRequested}
+						colorScheme="yellow"
+						onChange={(e) => setReceiptRequested(e.target.checked)}
+						>
+						현금영수증 신청
+						</Checkbox>
+						<FormControl>
+						<FormLabel>현금영수증 종류</FormLabel>
+						<Select value={receiptType} onChange={handleReceiptTypeChange}>
+							<option value="개인소득공제">개인소득공제</option>
+							<option value="사업자지출증빙">사업자지출증빙</option>
+						</Select>
+						</FormControl>
+						<FormControl>
+							<FormLabel>현금영수증 번호</FormLabel>
+							<Input
+								placeholder="(-없이) 숫자만 입력해주세요."
+								value={receiptNumber}
+								onChange={(e) => setReceiptNumber(e.target.value)}
+							/>
+						</FormControl>
+						<Divider />
+						<HStack w="100%" justifyContent="space-between">
+						<Text fontWeight="bold">최종 결제금액</Text>
+						<Text fontSize="2xl" fontWeight="bold">{data?.options.productPrice}원</Text>
+						</HStack>
+						<Button type="submit" colorScheme="yellow" size="lg" w="100%">
+						{data?.options.productPrice}원 결제하기
+						</Button>
+					</Stack>
 					</VStack>
 				</HStack>
-				</Box>
-			</Box>
-			</VStack>
-			<VStack spacing={8} align="stretch" flex={1}>
-			<Text fontSize="2xl" fontWeight="bold" mb={4}>결제 정보</Text>
-			<Stack spacing={4}>
-				<Checkbox
-				isChecked={receiptRequested}
-				colorScheme="yellow"
-				onChange={(e) => setReceiptRequested(e.target.checked)}
-				>
-				현금영수증 신청
-				</Checkbox>
-				<FormControl>
-				<FormLabel>현금영수증 종류</FormLabel>
-				<Select value={receiptType} onChange={handleReceiptTypeChange}>
-					<option value="개인소득공제">개인소득공제</option>
-					<option value="사업자지출증빙">사업자지출증빙</option>
-				</Select>
-				</FormControl>
-				<FormControl>
-					<FormLabel>현금영수증 번호</FormLabel>
-					<Input
-						placeholder="(-없이) 숫자만 입력해주세요."
-						value={receiptNumber}
-						onChange={(e) => setReceiptNumber(e.target.value)}
-					/>
-				</FormControl>
-				<Divider />
-				<HStack w="100%" justifyContent="space-between">
-				<Text fontWeight="bold">최종 결제금액</Text>
-				<Text fontSize="2xl" fontWeight="bold">{data?.options.productPrice}원</Text>
-				</HStack>
-				<Button colorScheme="yellow" size="lg" w="100%">
-				{data?.options.productPrice}원 결제하기
-				</Button>
-			</Stack>
-			</VStack>
-		</HStack>
+			</form>
 		</Box>}
 		</>
 	);
