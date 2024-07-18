d005bd3fcf31f7e3c346913497606ad9e74913a8
loveydev <tjgus9650@gmail.com>
2024-07-18 18:13:46 +0900

    Chore: fetchInstance 를 사용한 코드로 수정

--- src/api/hooks/useGetProductsInfo.ts
+++ src/api/hooks/useGetProductsInfo.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
 
 import type { ProductsInfoData } from '@/types'
+
+import { fetchInstance } from '../instance'
 
 const getProductsPath = (product: string) => `/v1/products/${product}/detail`
 
 export const getProducts = async (product: string) => {
-  const response = await axios.get<ProductsInfoData>(getProductsPath(product))
+  const response = await fetchInstance.get<ProductsInfoData>(getProductsPath(product))
   return response.data.detail
 }
 
