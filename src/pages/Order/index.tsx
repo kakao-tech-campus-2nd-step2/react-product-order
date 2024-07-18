754e52710cb86f0e18752dd257541380950d68b6
Dobbymin <gmin0701@knu.ac.kr>
2024-07-19 02:13:44 +0900

    feat(order):  uri 접속시 redirect 기능 구현

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,13 +1,35 @@
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { Container } from '@/components/common/layouts/Container';
 import OrderBuySection from '@/components/features/Order/OrderBuySection';
 import OrderDetailSection from '@/components/features/Order/OrderDetailSection';
+import { RouterPath } from '@/routes/path';
 import { Box } from '@chakra-ui/react';
 
+type LocationState = {
+  title: string;
+  amount: number;
+  imageSrc: string;
+  subtitle: string;
+  quantity: number;
+};
+
 const OrderPage = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const { title, imageSrc, amount, subtitle } = location.state;
+
+  useEffect(() => {
+    if (!location.state) {
+      navigate(RouterPath.home);
+    }
+  }, [location, navigate]);
+
+  if (!location.state) {
+    return null;
+  }
+  const { title, amount, imageSrc, subtitle, quantity }: LocationState =
+    location.state;
   return (
     <form action=''>
       <Box width='100%'>
@@ -23,8 +45,9 @@ const OrderPage = () => {
               imageSrc={imageSrc}
               title={title}
               subtitle={subtitle}
+              quantity={quantity}
             />
-            <OrderBuySection price={amount} />
+            <OrderBuySection price={amount * quantity} />
           </Box>
         </Container>
       </Box>
