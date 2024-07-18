d98a9c9adc1ba0d921da7142fad87a92e5e4780d
unknown <kc21ynje@gmail.com>
2024-07-19 05:42:42 +0900

    feat: add product order page

---
+++ src/components/features/Order/OrderMain.tsx
@@ -0,0 +1,116 @@
+import { Box, Flex, Image, Text, Textarea } from "@chakra-ui/react";
+
+import type { ProductDetailResponseData } from "@/api/hooks/useGetProductDetail";
+
+export const OrderMain = (
+  quantity: number,
+  data: ProductDetailResponseData,
+  message: string,
+  setMessage: React.Dispatch<React.SetStateAction<string>>,
+) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(event.target.value);
+  };
+
+  return (
+    <Box width="100%" maxWidth="900px">
+      <Box
+        borderLeft="1px solid rgb(229, 229, 229)"
+        height="calc(-54px + 100vh)"
+        padding="44px 0px 32px"
+      >
+        <Box>
+          <Box width="100%" display="flex" justifyContent="center">
+            <Text
+              fontSize="18px"
+              lineHeight="21px"
+              color="rgb(34, 34, 34)"
+              fontWeight="700"
+              boxSizing="border-box"
+            >
+              나에게 주는 선물
+            </Text>
+          </Box>
+          <Box width="100%" padding="14px 30px">
+            <Box width="100%" padding="12px 30px 16px">
+              <Textarea
+                name="messageCardTextMessage"
+                placeholder="선물과 함께 보낼 메시지를 적어보세요"
+                fontSize="md"
+                paddingInlineStart="4"
+                paddingInlineEnd="4"
+                borderRadius="md"
+                minWidth="0px"
+                outline="transparent solid 2px"
+                outlineOffset="2px"
+                position="relative"
+                appearance="none"
+                transition="common 200ms"
+                paddingTop="2"
+                paddingBottom="2"
+                minHeight="10"
+                lineHeight="short"
+                verticalAlign="top"
+                borderWidth="2px"
+                borderStyle="solid"
+                borderColor="transparent"
+                backgroundColor="gray.100"
+                resize="none"
+                height="100px"
+                value={message}
+                onChange={handleInputChange}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box height="8px" backgroundColor="#EDEDED" />
+        <Box width="100%" padding="16px">
+          <Text fontSize="15px" lineHeight="24px" fontWeight="700" color="black">
+            선물내역
+          </Text>
+          <Box height="16px" />
+          <Flex
+            width="100%"
+            padding="20px 16px 16px"
+            borderRadius="8px"
+            border="1px solid"
+            borderColor="gray.200"
+            boxShadow="0px 4px 8px rgba(0, 0, 0, 0.02)"
+          >
+            <Box
+              border="1px solid"
+              borderColor="blackAlpha.50"
+              borderRadius="4px"
+              overflow="hidden"
+              width="86px"
+            >
+              <Image
+                src={data.detail.imageURL}
+                width="86px"
+                objectFit="cover"
+                objectPosition="center center"
+                borderRadius="0px"
+                aspectRatio="1 / 1"
+              />
+            </Box>
+            <Box pl="8px">
+              <Text fontSize="13px" lineHeight="14px" color="gray.500" fontWeight="normal">
+                {data.detail.brandInfo.name}
+              </Text>
+              <Text
+                fontSize="14px"
+                lineHeight="18px"
+                marginTop="3px"
+                color="gray.900" // Use Chakra UI's gray color palette
+                overflow="hidden"
+                fontWeight="normal"
+              >
+                {data.detail.name} X {quantity}개
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,11 +1,67 @@
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
+import { OrderAside } from "@/components/features/Order/OrderAside";
+import { OrderMain } from "@/components/features/Order/OrderMain";
 import { RouterPath } from "@/routes/path";
 
 export const OrderPage = () => {
   const location = useLocation();
   const { state } = location;
+  const [message, setMessage] = useState("");
+  const [isChecked, setChecked] = useState(false);
+  const [cashReceiptType, setCashReceiptType] = useState("PERSONAL");
+  const [cashReceiptNumber, setCashReceiptNumber] = useState("");
 
   if (!state) return <Navigate to={RouterPath.notFound} />;
-  return <>주문 페이지</>;
+
+  const quantity = state?.quantity;
+  const data = state?.data;
+
+  const handleSubmit = () => {
+    if (!message) {
+      alert("메시지를 입력해주세요!");
+      return;
+    }
+    if (isChecked) {
+      if (
+        isNaN(Number(cashReceiptNumber)) ||
+        Number(cashReceiptNumber).toString() != cashReceiptNumber
+      ) {
+        alert("현금영수증 번호는 숫자로만 입력해주세요.");
+        return;
+      }
+    }
+
+    alert("주문이 완료되었습니다.");
+    return;
+  };
+
+  return (
+    <Flex width="100%" justifyContent="center" alignItems="center">
+      <Flex
+        width="100%"
+        maxWidth="1280px"
+        flexDirection="column"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <Flex width="100%" justifyContent="flex-start" alignItems="flex-start" position="relative">
+          {OrderMain(quantity, data, message, setMessage)}
+          {OrderAside(
+            quantity,
+            data,
+            isChecked,
+            setChecked,
+            cashReceiptType,
+            setCashReceiptType,
+            cashReceiptNumber,
+            setCashReceiptNumber,
+            handleSubmit,
+          )}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
