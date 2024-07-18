76612bc78a1e4df1bbaee737b75abb8ee25b603f
Dobbymin <gmin0701@knu.ac.kr>
2024-07-19 02:12:14 +0900

    feat(product.api): product option api 호출 코드 구현

---
+++ src/api/hooks/useGetProductsOptions.ts
@@ -0,0 +1,36 @@
+import { fetchInstance } from '../instance';
+import { useQuery } from '@tanstack/react-query';
+
+type RequestParams = {
+  productId: string;
+};
+
+type ProductsOptionsResponseData = {
+  options: {
+    productId: number;
+    productName: string;
+    productPrice: number;
+    hasOption: boolean;
+    giftOrderLimit: number;
+    names: string[];
+    options: string[];
+  };
+};
+
+const getProductsOptionsPath = ({ productId }: RequestParams) => {
+  return `/v1/products/${productId}/options`;
+};
+
+export const getProductsOptions = async (params: RequestParams) => {
+  const response = await fetchInstance.get<ProductsOptionsResponseData>(
+    getProductsOptionsPath(params)
+  );
+  return response.data;
+};
+
+export const useGetProductOptions = ({ productId }: RequestParams) => {
+  return useQuery({
+    queryKey: ['productsOptions', productId],
+    queryFn: () => getProductsOptions({ productId }),
+  });
+};
