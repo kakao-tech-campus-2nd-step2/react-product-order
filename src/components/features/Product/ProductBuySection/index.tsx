2072ad2db7856df37f8e57162f1982da210d5f26
Dobbymin <gmin0701@knu.ac.kr>
2024-07-19 02:15:30 +0900

    feat(order): 물건 최대 갯수 제한 기능 구현

--- src/components/features/Product/ProductBuySection/index.tsx
+++ src/components/features/Product/ProductBuySection/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as S from './styles';
+import { useAuth } from '@/provider/Auth';
 import { RouterPath } from '@/routes/path';
 import { Text, Button, Input } from '@chakra-ui/react';
 
@@ -9,19 +11,43 @@ type Props = {
   amount: number;
   imageSrc: string;
   subtitle: string;
+  limit: number;
 };
-const ProductBuySection = ({ title, amount, imageSrc, subtitle }: Props) => {
+const ProductBuySection = ({
+  title,
+  amount,
+  imageSrc,
+  subtitle,
+  limit,
+}: Props) => {
+  const [quantity, setQuantity] = useState(1);
   const navigate = useNavigate();
+  const authInfo = useAuth();
 
   const handleOrder = () => {
-    navigate(RouterPath.orders, {
-      state: {
-        title,
-        imageSrc,
-        amount,
-        subtitle,
-      },
-    });
+    if (!authInfo) {
+      const confirmed = confirm(
+        '로그인이 필요한 메뉴입니다. \n로그인 페이지로 이동하시겠습니까?'
+      );
+      if (confirmed) {
+        navigate(RouterPath.login);
+      }
+      return;
+    } else {
+      navigate(RouterPath.orders, {
+        state: { title, amount, imageSrc, subtitle, quantity },
+      });
+    }
+  };
+
+  const handleIncrease = () => {
+    setQuantity((prevQuantity) =>
+      prevQuantity < limit ? prevQuantity + 1 : limit
+    );
+  };
+
+  const handleDecrease = () => {
+    setQuantity((prevQuantity) => (prevQuantity > 1 ? prevQuantity - 1 : 1));
   };
   return (
     <S.AsideBox>
@@ -42,7 +68,8 @@ const ProductBuySection = ({ title, amount, imageSrc, subtitle }: Props) => {
               aria-label='수량 1개 감소'
               role='button'
               tabIndex={-1}
-              aria-disabled='true'
+              aria-disabled={quantity <= 1}
+              onClick={handleDecrease}
             >
               <Text fontSize='24px' fontWeight='700'>
                 -
@@ -55,18 +82,20 @@ const ProductBuySection = ({ title, amount, imageSrc, subtitle }: Props) => {
               role='spinbutton'
               aria-valuemin={1}
               aria-valuemax={100}
-              aria-valuenow={1}
-              aria-valuetext='1'
+              aria-valuenow={quantity}
+              aria-valuetext={`${quantity}`}
               autoComplete='off'
               autoCorrect='off'
-              value='1'
+              value={quantity}
+              readOnly
             />
             <Button
               type='button'
               aria-label='수량 1개 추가'
               role='button'
               tabIndex={-1}
-              aria-disabled='true'
+              aria-disabled={quantity >= 100}
+              onClick={handleIncrease}
             >
               <Text fontSize='20px' fontWeight='700'>
                 +
@@ -76,7 +105,7 @@ const ProductBuySection = ({ title, amount, imageSrc, subtitle }: Props) => {
         </S.ProductNumberBox>
         <S.ButtonBox>
           <S.TotalPriceBox>
-            총 결제 금액 <S.PriceText>{amount}원</S.PriceText>
+            총 결제 금액 <S.PriceText>{amount * quantity}원</S.PriceText>
           </S.TotalPriceBox>
           <S.ButtonItem onClick={handleOrder}>나에게 선물하기</S.ButtonItem>
         </S.ButtonBox>
