634677504a842210bf69fe79067858a1c6fd3f26
Hyoeunkh <lhe4528@knu.ac.kr>
2024-07-18 16:15:32 +0900

    feat: chakra ui 설치 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { MainLayout } from '@/components/Layout/MainLayout';
 import LoginPage from '@/pages/LoginPage';
 import MainPage from '@/pages/MainPage';
 import MyAccountPage from '@/pages/MyAccountPage';
+import OrderPage from '@/pages/OrderPage';
+import ProductDetailPage from '@/pages/ProductDetailPage';
 import ThemePage from '@/pages/ThemePage';
 import { ResetStyles } from '@/styles/reset';
 
@@ -34,6 +36,8 @@ const App = () => {
               }
             />
             <Route path="/theme/:themeKey" element={<ThemePage />} />;
+            <Route path="/products/:productId" element={<ProductDetailPage />} />;
+            <Route path="/order" element={<OrderPage />} />;
           </Route>
         </Routes>
       </AuthProvider>
