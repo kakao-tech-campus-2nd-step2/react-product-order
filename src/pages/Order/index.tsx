feaebd11dfbfe03d882dd9d7111cd9673dd68095
JaeAnHan <jaean1999@naver.com>
2024-07-19 18:18:29 +0900

    feat: 현금 영수증 유효한지 확인 및 유효하지 않다면 메세지 표시

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,3 +1,6 @@
+import { createPortal } from 'react-dom';
+
+import { OrderToast } from '@/components/features/Order/atoms/OrderToast';
 import { OrderListWithMessage } from '@/components/features/Order/organisms/OrderListWithMessage';
 import { OrderReceipt } from '@/components/features/Order/organisms/OrderReceipt';
 import { ProductTemplate } from '@/components/templates/ProductTemplate';
@@ -6,7 +9,7 @@ import { useOrderPage } from '@/hooks/useOrderPage';
 export const orderLetterPlaceHolder = `선물과 함께 보낼 메시지를 적어보세요`;
 
 export const OrderPage = () => {
-  const { onSubmit, memState, setMessage, cacheReceiptRefs } = useOrderPage();
+  const { warning, onSubmit, memState, setMessage, cacheReceiptRefs } = useOrderPage();
   return (
     <form onSubmit={onSubmit}>
       <ProductTemplate
@@ -15,6 +18,7 @@ export const OrderPage = () => {
           <OrderReceipt cacheReceiptRefs={cacheReceiptRefs} totalPrice={memState.totalPrice} />
         }
       />
+      {warning && createPortal(<OrderToast warning={warning} />, document.body)}
     </form>
   );
 };
