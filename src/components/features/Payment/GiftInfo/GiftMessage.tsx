ef591588edec06aa31ec1468bcf96c7b2ca90a45
yunseong0404 <333665@naver.com>
2024-07-20 04:42:21 +0900

    feat: Payment page 구현

---
+++ src/components/features/Payment/GiftInfo/GiftMessage.tsx
@@ -0,0 +1,28 @@
+import { Box, Text, Textarea } from '@chakra-ui/react';
+
+export default function GiftMessage() {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      p="44px auto 24px"
+      width="841px"
+      height="253px"
+    >
+      <Text fontSize="18px" fontWeight={700} mb={4}>
+        나에게 주는 선물
+      </Text>
+      <Textarea
+        p="12px 30px 16px"
+        bgColor="rgb(226, 232, 240)"
+        width="780px"
+        height="100px"
+        placeholder="선물과 함께 보낼 메시지를 적어보세요."
+        mb={6}
+      />
+      <Box bgColor="rgb(237, 237, 237)" height="8px" width="899px"></Box>
+    </Box>
+  );
+}
