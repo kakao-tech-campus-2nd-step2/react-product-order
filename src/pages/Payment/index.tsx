0baa4b46fae384dcd5c10c0208ad452290fd787c
Solbin Jung <jsolbin0724@gmail.com>
2024-07-19 10:37:43 +0900

    feat: 선택한 상품 내역 표시

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -3,10 +3,20 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  Image,
+  Text,
   Textarea
 } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 
 export const PaymentPage = () => {
+  const location = useLocation();
+  const { name, imageURL } = location.state as {
+    name: string;
+    imageURL: string;
+    totalPrice: number;
+  };
+
   return (
     <Box p={8}>
       <Flex direction={{ base: 'column', md: 'row' }} alignItems={{ base: 'center', md: 'flex-start' }} maxWidth="1200px" mx="auto">
@@ -15,6 +25,13 @@ export const PaymentPage = () => {
             <FormLabel fontWeight="bold">나에게 주는 선물</FormLabel>
             <Textarea placeholder="선물과 함께 보낼 메시지를 적어보세요" size="xl" h="200px" w="100%" />
           </FormControl>
+          <Text fontWeight="bold" mb={2}>선물내역</Text>
+          <Flex>
+            <Image src={imageURL} alt={name} mb={4} maxW="100px" />
+            <Box ml={4}>
+              <Text fontSize="lg">{name}</Text>
+            </Box>
+          </Flex>
         </Box>
       </Flex>
     </Box>
