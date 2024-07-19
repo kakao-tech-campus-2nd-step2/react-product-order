6439134f5d2b3908d596e0f95b2d44bf2f96b71f
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-19 16:09:17 +0900

    feat: implement product options API

--- src/apis/products/index.ts
+++ src/apis/products/index.ts
@@ -1,14 +1,18 @@
-import { ProductDetailRequest } from '@internalTypes/requestTypes';
+import { ProductDetailRequest, ProductOptionsRequest } from '@internalTypes/requestTypes';
 import { ProductDetailResponse } from '@internalTypes/responseTypes';
 import axiosInstance from '../instance';
 import { PRODUCTS_PATHS } from './path';
 
-export const getProductsDetail = async (params?: ProductDetailRequest): Promise<ProductDetailResponse> => {
-  if (!params || !params.productId) {
-    throw new Error('ProductId is required');
-  }
-
+export const getProductsDetail = async (params: ProductDetailRequest): Promise<ProductDetailResponse> => {
   const { productId } = params;
+
   const res = await axiosInstance.get<ProductDetailResponse>(PRODUCTS_PATHS.PRODUCTS_DETAIL(productId));
+  return res.data;
+};
+
+export const getProductsOptions = async (params: ProductOptionsRequest) => {
+  const { productId } = params;
+
+  const res = await axiosInstance.get<ProductDetailResponse>(PRODUCTS_PATHS.PRODUCTS_OPTIONS(productId));
   return res.data;
 };
