8214bd3ea81975fe964651b8cd8b32ba4afaf7a1
yunseong0404 <333665@naver.com>
2024-07-19 14:39:31 +0900

    feat: GiftButton UI 구현

---
+++ src/components/features/GoodsDetail/SideBar/GiftButton.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@chakra-ui/react';
+
+export default function GiftButton() {
+  return (
+    <div>
+      <Button
+        color="#fff"
+        fontSize="16px"
+        bgColor="rgb(17, 17, 17)"
+        borderRadius="4px"
+        height="60px"
+        width="318px"
+        p="0"
+      >
+        나에게 선물하기
+      </Button>
+    </div>
+  );
+}
