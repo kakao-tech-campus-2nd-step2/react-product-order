9e82ed3220cf24f703ab14dd1e43189c87b019db
yunseong0404 <333665@naver.com>
2024-07-19 14:40:07 +0900

    feat: GoodsCount UI 구현

---
+++ src/components/features/GoodsDetail/SideBar/GoodsCount.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, Flex, NumberInput, NumberInputField, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+export default function GoodsCount() {
+  const [amount, setAmount] = useState(1);
+
+  const handleIncrease = () => {
+    setAmount(Math.min(amount + 1, 100));
+  };
+
+  const handleDecrease = () => {
+    setAmount(Math.max(amount - 1, 1));
+  };
+
+  const handleChange = (value: string) => {
+    setAmount(Number(value));
+  };
+
+  return (
+    <div>
+      <Box
+        p="12px 14px 16px"
+        height="100px"
+        width="318px"
+        borderWidth="0.1px"
+        borderColor="rgb(237, 237, 237)"
+      >
+        <Box fontSize="16px" fontWeight={700}>
+          외식 통합권 10만원권
+        </Box>
+        <Flex pt="8px" justifyContent="space-between" alignItems="center">
+          <Button
+            borderRadius="6px"
+            padding="0"
+            onClick={handleDecrease}
+          >
+            <Text display="flex" fontSize="16px" fontWeight={600} color="rgb(26, 32, 44)">
+              -
+            </Text>
+          </Button>
+          <NumberInput
+            height="40px"
+            width="192px"
+            borderRadius="6px"
+            defaultValue={1}
+            min={1}
+            max={100}
+            id="amount"
+            name="amount"
+            value={amount}
+            onChange={(value) => handleChange(value)}
+          >
+            <NumberInputField padding="0px 12px"></NumberInputField>
+          </NumberInput>
+          <Button
+            borderRadius="6px"
+            padding="0"
+            onClick={handleIncrease}
+          >
+            <Text display="flex" fontSize="16px" fontWeight={600} color="rgb(26, 32, 44)">+</Text>
+            
+          </Button>
+        </Flex>
+      </Box>
+    </div>
+  );
+}
