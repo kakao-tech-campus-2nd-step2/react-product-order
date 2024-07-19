04db523bfbc248ac0ca6bcbd6c64e66603e9675a
yunseong0404 <333665@naver.com>
2024-07-19 23:39:33 +0900

    fix: GoodDetail page 구현

--- src/pages/GoodsDetail/GoodsDetail.tsx
+++ src/pages/GoodsDetail/GoodsDetail.tsx
@@ -1,14 +1,30 @@
+import { Box, Container } from '@chakra-ui/react';
+import { Navigate, useParams } from 'react-router-dom';
 
-import { Container } from "@chakra-ui/react";
+import useGetProductsDetail from '@/api/hooks/useGetProductsDetail';
+import { Spinner } from '@/components/common/Spinner';
+import GoodsInfo from '@/components/features/GoodsDetail/GoodsInfo';
+import SideBar from '@/components/features/GoodsDetail/SideBar/SideBar';
+import { RouterPath } from '@/routes/path';
+import type { ProductDetailParams } from '@/types';
 
-import GoodsInfo from "@/components/features/GoodsDetail/GoodsInfo";
-import SideBar from "@/components/features/GoodsDetail/SideBar/SideBar";
+export const GoodsDetailPage = () => {
+  const { id = '' } = useParams<ProductDetailParams>();
+  const { data, isLoading, isError } = useGetProductsDetail({ id });
 
-export default function GoodsDetail() {
+  if (isLoading)
+    return (
+      <Box>
+        <Spinner />
+      </Box>
+    );
+  if (isError) return <Box>에러가 발생했습니다.</Box>;
+  if (!data) return <Navigate to={RouterPath.home} />;
+
   return (
     <Container display="flex" flexDirection="row" justifyContent="center">
-      <GoodsInfo/>
-      <SideBar/>
+      <GoodsInfo price={data.price.sellingPrice} imageURL={data.imageURL} name={data.name} />
+      <SideBar price={data.price.sellingPrice} name={data.name} />
     </Container>
-  )
-}
+  );
+};
