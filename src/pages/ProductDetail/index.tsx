4faaf14eb8e722ac9f600e2e724669a3fd7fcf8b
Kuheewon <selly3109@naver.com>
2024-07-19 20:39:14 +0900

    refactor. 코드 리팩토링

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { useGetProductDetail } from '@/api/hooks/useGetProductDetail';
 import { Spinner } from '@/components/common/Spinner';
+import { ProductDetailContent } from '@/components/features/ProductDetail/ProductDetail/ProductDetailContent';
 import { useAuth } from '@/provider/Auth';
 
 const ProductDetail: React.FC = () => {
@@ -36,80 +36,18 @@ const ProductDetail: React.FC = () => {
   if (!data) return <TextView>상품이 없습니다.</TextView>;
 
   const product = data.detail;
-  const imageUrl = product.imageURL;
-  const sellingPrice = product.price?.sellingPrice;
 
   return (
-    <Container>
-      <ImageContainer>
-        <Image src={imageUrl} alt={product.name} />
-      </ImageContainer>
-      <DetailsContainer>
-        <Title>{product.name}</Title>
-        <Price>{sellingPrice}원</Price>
-        <Description>카톡 친구가 아니어도 선물 코드로 선물 할 수 있어요!</Description>
-        <ButtonContainer>
-          <Button colorScheme="teal" variant="outline" onClick={handleGiftButtonClick}>
-            나에게 선물하기
-          </Button>
-        </ButtonContainer>
-      </DetailsContainer>
-    </Container>
+    <ProductDetailContent
+      imageUrl={product.imageURL}
+      name={product.name}
+      price={product.price?.sellingPrice}
+      onButtonClick={handleGiftButtonClick}
+    />
   );
 };
 
 export default ProductDetail;
-
-const Container = styled.div`
-  display: flex;
-  padding: 20px;
-`;
-
-const ImageContainer = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Image = styled.img`
-  max-width: 80%;
-  height: auto;
-  border-radius: 8px;
-`;
-
-const DetailsContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  padding-left: 20px;
-  padding-top: 20px;
-`;
-
-const Title = styled.h2`
-  margin: 0;
-  font-size: 24px;
-  font-weight: bold;
-`;
-
-const Price = styled.p`
-  margin-top: 10px;
-  font-size: 24px;
-  color: red;
-  font-weight: bold;
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 20px;
-  margin-bottom: 20px;
-`;
-
-const Description = styled.p`
-  margin-top: 20px;
-  font-size: 16px;
-  color: #555;
-`;
 
 const TextView = styled.div`
   width: 100%;
