31c857c0cb5b17496bfe645ed68941b7144bd4ca
loveydev <tjgus9650@gmail.com>
2024-07-19 22:52:24 +0900

    Feat: react-hook-form 을 이용하여 form 및 input 변경

--- src/components/features/Order/index.tsx
+++ src/components/features/Order/index.tsx
@@ -1,22 +1,23 @@
 import { Box, Button, Checkbox, Grid, GridItem, HStack, Image, Input, Select, Text, VStack } from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import { useRef, useState } from 'react';
+import type { SubmitHandler} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
+
+interface FormValues {
+  number?: string;
+  message?: string;
+  receiptType?: string;
+  cashReceipt?: boolean;
+}
 
 export const Order = () => {
   const location = useLocation();
-  const [check, setCheck] = useState(false);
+  const { register, handleSubmit, watch } = useForm<FormValues>();
+  const check = watch('cashReceipt');
 
-  const numberRef = useRef<HTMLInputElement>(null);
-  const messageRef = useRef<HTMLInputElement>(null);
-
-  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCheck(e.target.checked);
-  };
-
-  const handleFinish = () => {
-    const number = numberRef.current?.value;
-    const message = messageRef.current?.value;
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    const { number, message } = data;
 
     // 카드 메시지가 100자를 초과할 경우 경고 표시
     if (message && message.length > 100) {
@@ -27,7 +28,7 @@ export const Order = () => {
     // 현금 영수증 신청을 한 경우 (체크 박스 활성화 상태)
     if (check) {
       if (number && message) {
-        if (Number.isNaN(Number(number))) {
+        if (isNaN(Number(number))) {
           alert('현금 영수증 전화번호는 숫자만 입력 가능합니다!');
         } else {
           alert('주문이 완료되었습니다.');
@@ -59,32 +60,34 @@ export const Order = () => {
         p={4}
       >
         <GridItem>
-          <VStack align="center" spacing={10}>
-            <Box textAlign="center">
+          <VStack align="start" spacing={10}>
+            <Box>
               <Text fontSize="2xl" fontWeight="bold">나에게 주는 선물</Text>
-              <Input
-                backgroundColor="gray.100"
-                placeholder="선물과 함께 보낼 메시지를 적어보세요"
-                ref={messageRef}
-                mt={4}
-                size="lg"
-                width="750px" 
-                height="150px" 
-              />
-            </Box>
-            <Box w="100%">
-              <Text fontSize="xl" fontWeight="bold">선물 내역</Text>
-              <GiftDetailBox>
-                <Image src={location.state.data.detail.imageURL} alt={location.state.data.detail.name} width="150px" height="150px" />
-                <Box>
-                  <Text fontSize="lg" fontWeight="semibold" color="gray.700">
-                    {location.state.data.detail.brandInfo.name}
-                  </Text>
-                  <Text fontSize="md" fontWeight="medium" color="gray.500">
-                    {location.state.data.detail.name} x {location.state.itemCount}개
-                  </Text>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                  backgroundColor="gray.100"
+                  placeholder="선물과 함께 보낼 메시지를 적어보세요"
+                  {...register('message')}
+                  mt={4}
+                  size="lg"
+                  width="750px" 
+                  height="150px"
+                />
+                <Box mt={4}>
+                  <Text fontSize="xl" fontWeight="bold">선물 내역</Text>
+                  <GiftDetailBox>
+                    <Image src={location.state.data.detail.imageURL} alt={location.state.data.detail.name} width="150px" height="150px" />
+                    <Box>
+                      <Text fontSize="lg" fontWeight="semibold" color="gray.700">
+                        {location.state.data.detail.brandInfo.name}
+                      </Text>
+                      <Text fontSize="md" fontWeight="medium" color="gray.500">
+                        {location.state.data.detail.name} x {location.state.itemCount}개
+                      </Text>
+                    </Box>
+                  </GiftDetailBox>
                 </Box>
-              </GiftDetailBox>
+              </form>
             </Box>
           </VStack>
         </GridItem>
@@ -93,27 +96,27 @@ export const Order = () => {
           <PaymentBox>
             <Text fontSize="xl" fontWeight="bold">결제 정보</Text>
             <VStack spacing={4} align="start" mt={4}>
-              <Checkbox onChange={handleCheck}>현금영수증 신청</Checkbox>
+              <Checkbox {...register('cashReceipt')}>현금영수증 신청</Checkbox>
               {check && (
                 <>
-                  <Select placeholder="현금 영수증 종류를 선택하세요">
+                  <Select placeholder="현금 영수증 종류를 선택하세요" {...register('receiptType')}>
                     <option value="personal">개인 소득 공제</option>
                     <option value="business">사업자 증빙용</option>
                   </Select>
                   <Input
                     placeholder="(-없이) 숫자만 입력해주세요."
-                    ref={numberRef}
+                    {...register('number', { pattern: /^[0-9]+$/ })}
                   />
                 </>
               )}
-              <Box w="100%">
+              <Box w="100%" mt={4}>
                 <HStack justify="space-between">
                   <Text fontSize="lg">최종 결제 금액</Text>
                   <Text fontSize="2xl" fontWeight="bold">{location.state.totalPrice} 원</Text>
                 </HStack>
               </Box>
-              <Button onClick={handleFinish} size="sm" width="90%" mt={10}>
-                {location.state.totalPrice} 원 결제하기
+              <Button type="submit" size="sm" width="90%" mt={10}>
+                  {location.state.totalPrice} 원 결제하기
               </Button>
             </VStack>
           </PaymentBox>
--- src/components/features/Products/ProductsDetail/index.tsx
+++ src/components/features/Products/ProductsDetail/index.tsx
@@ -1,89 +1,91 @@
 import { Box, Button, Grid, GridItem, Input, Text } from '@chakra-ui/react';
-import styled from "@emotion/styled"
-import { useEffect, useState } from "react"
-import { Navigate, useNavigate } from "react-router-dom"
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { Controller,useForm } from 'react-hook-form';
+import { Navigate, useNavigate } from "react-router-dom";
 
-import { useGetProductsInfo } from "@/api/hooks/useGetProductsInfo"
-import { Button as CustomBtn } from "@/components/common/Button"
-import { Image } from "@/components/common/Image"
-import { Spinner } from "@/components/common/Spinner"
-import { useAuth } from "@/provider/Auth"
-import { getDynamicPath, RouterPath } from "@/routes/path"
+import { useGetProductsInfo } from "@/api/hooks/useGetProductsInfo";
+import { Button as CustomBtn } from "@/components/common/Button";
+import { Image } from "@/components/common/Image";
+import { Spinner } from "@/components/common/Spinner";
+import { useAuth } from "@/provider/Auth";
+import { getDynamicPath, RouterPath } from "@/routes/path";
 
 type Props = {
-  productId: string
+  productId: string;
+}
+
+type FormValues = {
+  itemCount: number;
 }
 
 export const ProductsDetail = ({ productId }: Props) => {
-  const { data, isLoading, isError } = useGetProductsInfo(productId)
-  const [itemCount, setItemCount] = useState<number>(1)
-  const [totalPrice, setTotalPrice] = useState<number>(0)
-  const authInfo = useAuth()
-  const navigate = useNavigate()
+  const { data: productData, isLoading, isError } = useGetProductsInfo(productId);
+  const { control, handleSubmit, setValue, watch } = useForm<FormValues>({
+    defaultValues: { itemCount: 1 }
+  });
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const authInfo = useAuth();
+  const navigate = useNavigate();
 
-  // 선물 최대 제한 수량 
-  const giftOrderLimit = data?.options?.giftOrderLimit ?? 0
+  const watchItemCount = watch('itemCount');
+  const giftOrderLimit = productData?.options?.giftOrderLimit ?? 0;
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Number(event.target.value)
-    if (isNaN(value)) {
-      return
-    }
-
-    if (value > giftOrderLimit) {
-      alert(`최대 선물 가능 수량은 ${giftOrderLimit}개 입니다.`)
-      setItemCount(giftOrderLimit)
-    } 
-    else {
-      setItemCount(value)
-    }
-  }
-
-  const handleIncrement = () => {
-    if (itemCount < giftOrderLimit) {
-      setItemCount(itemCount + 1)
-    } else {
-      alert(`최대 선물 가능 수량은 ${giftOrderLimit}개 입니다.`)
-    }
-  }
-
-  const handleDecrement = () => {
-    if (itemCount > 1) {
-      setItemCount(itemCount - 1)
-    }
-  }
-
-  const handleNavigate = () => {
+  const onSubmit = (formValues: FormValues) => {
     if (!authInfo) {
-      const check = window.confirm('로그인이 필요한 서비스 입니다. 로그인 페이지로 이동하시겠습니까?') 
+      const check = window.confirm('로그인이 필요한 서비스 입니다. 로그인 페이지로 이동하시겠습니까?');
       if (check) {
-        navigate(getDynamicPath.login())
+        navigate(getDynamicPath.login());
       }
     } else {
-      navigate(RouterPath.order, { state: { itemCount, totalPrice, data } })
+      navigate(RouterPath.order, { state: { itemCount: formValues.itemCount, totalPrice, data: productData } });
     }
-  }
+  };
 
   useEffect(() => {
-    if (data) {
-      setTotalPrice(data.detail.price.basicPrice * itemCount)
-      console.log(`상품명: ${data.detail.name} / 선물 최대 제한 수량: ${giftOrderLimit}`)
+    if (productData) {
+      const newTotalPrice = productData.detail.price.basicPrice * watchItemCount;
+      setTotalPrice(newTotalPrice);
+      console.log(`상품명: ${productData.detail.name} / 선물 최대 제한 수량: ${giftOrderLimit}`);
     }
-  }, [data, itemCount, giftOrderLimit])
+  }, [productData, watchItemCount, giftOrderLimit]);
 
   if (isLoading) {
     return (
       <CenteredBox>
         <Spinner />
       </CenteredBox>
-    )
+    );
   }
 
   if (isError) {
     return (
       <Navigate to={RouterPath.notFound} />
-    )
+    );
   }
+
+  const handleChange = (value: number) => {
+    if (value > giftOrderLimit) {
+      alert(`최대 선물 가능 수량은 ${giftOrderLimit}개 입니다.`);
+      setValue('itemCount', giftOrderLimit);
+    } else {
+      setValue('itemCount', value);
+    }
+  };
+
+  const handleIncrement = () => {
+    if (watchItemCount < giftOrderLimit) {
+      setValue('itemCount', watchItemCount + 1);
+    } else {
+      alert(`최대 선물 가능 수량은 ${giftOrderLimit}개 입니다.`);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (watchItemCount > 1) {
+      setValue('itemCount', watchItemCount - 1);
+    }
+  };
 
   return (
     <Wrapper>
@@ -95,13 +97,13 @@ export const ProductsDetail = ({ productId }: Props) => {
         p={4}
       >
         <GridItem>
-          <Image src={data?.detail?.imageURL} alt={data?.detail?.name} />
+          <Image src={productData?.detail?.imageURL} alt={productData?.detail?.name} />
         </GridItem>
 
         <GridItem>
           <Box>
-            <Text fontSize="2xl" fontWeight="bold">{data?.detail?.name}</Text>
-            <Text fontSize="xl" color="gray.600">{data?.detail?.price.basicPrice} 원</Text>
+            <Text fontSize="2xl" fontWeight="bold">{productData?.detail?.name}</Text>
+            <Text fontSize="xl" color="gray.600">{productData?.detail?.price.basicPrice} 원</Text>
             <Text mt={4} fontSize="md" color="gray.500">
               카톡 친구가 아니어도 선물 코드로 선물 할 수 있어요!
             </Text>
@@ -110,14 +112,20 @@ export const ProductsDetail = ({ productId }: Props) => {
           <Box mt={6}>
             <QuantityControl>
               <Button size="sm" onClick={handleDecrement}>-</Button>
-              <Input
-                type="number"
-                width="60px"
-                textAlign="center"
-                value={itemCount}
-                onChange={handleChange}
-                mx={2}
-                min={1} // 입력란에서 최소 수량을 1로 설정
+              <Controller
+                name="itemCount"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    type="number"
+                    width="60px"
+                    textAlign="center"
+                    {...field}
+                    onChange={(e) => handleChange(Number(e.target.value))}
+                    mx={2}
+                    min={1}
+                  />
+                )}
               />
               <Button size="sm" onClick={handleIncrement}>+</Button>
             </QuantityControl>
@@ -129,14 +137,14 @@ export const ProductsDetail = ({ productId }: Props) => {
           </Box>
 
           <Box mt={6}>
-            <CustomBtn theme="black" size="large" onClick={handleNavigate}>
+            <CustomBtn theme="black" size="large" onClick={handleSubmit(onSubmit)}>
               나에게 선물하기
             </CustomBtn>
           </Box>
         </GridItem>
       </Grid>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
