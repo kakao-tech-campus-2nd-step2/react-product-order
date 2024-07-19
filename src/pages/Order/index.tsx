b100d8e47483a9e44026f3580f8d3dfa519eb7e4
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-19 23:06:05 +0900

    refactor: add useOrderData hooks

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import Layout from '@components/features/Layout';
 import { CenteredContainer } from '@components/common';
 import OrderMessage from '@components/features/Order/OrderMessage';
 import Payment from '@components/features/Order/Payment';
+import useOrderData from './hooks/useOrderData';
 
 export default function Order() {
-  const [orderData, setOrderData] = useState({
-    message: '',
-    hasCashRecipt: false,
-    cashReciptType: '개인소득공제',
-    cashReciptNumber: '',
-  });
-
-  const handleOrderDataChange = (name: string, value: any) => {
-    setOrderData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  const { orderData, handleOrderDataChange } = useOrderData();
 
   return (
     <Layout>
