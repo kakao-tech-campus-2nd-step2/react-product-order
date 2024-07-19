ace95b48d2d648ecfb0ba080f6bdee043959afc9
yunseong0404 <333665@naver.com>
2024-07-19 14:37:57 +0900

    fix: detail page 경로 추가

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -4,11 +4,13 @@ export const RouterPath = {
   theme: '/theme/:themeKey',
   myAccount: '/my-account',
   login: '/login',
+  goodsDetail: '/theme/:themeKey/:id',
   notFound: '*',
 };
 
 export const getDynamicPath = {
   theme: (themeKey: string) => RouterPath.theme.replace(':themeKey', themeKey),
+  detail: (themeKey: string, id: number) => RouterPath.goodsDetail.replace(':themeKey', themeKey).replace(':id', id.toString()), 
   login: (redirect?: string) => {
     const currentRedirect = redirect ?? window.location.href;
     return `${RouterPath.login}?redirect=${encodeURIComponent(currentRedirect)}`;
