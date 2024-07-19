fddc62d2deba42a4ab351fdfc9eb9b3cc1c05d7e
2021048695 <hanyanging@hanyang.ac.kr>
2024-07-20 00:18:07 +0900

    feat: type 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,4 @@
-export type ThemeData = {
-  id: number;
-  key: string;
-  label: string;
-  title: string;
-  description?: string;
-  backgroundColor: string;
-  imageURL: string;
-};
-
-export type RankingFilterOption = {
-  targetType: 'ALL' | 'FEMALE' | 'MALE' | 'TEEN';
-  rankType: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
-};
-
-export type GoodsData = {
+export type ProductDetails = {
   id: number;
   name: string;
   imageURL: string;
@@ -31,4 +16,61 @@ export type GoodsData = {
     name: string;
     imageURL: string;
   };
+  isAccessableProductPage: boolean;
+  review: {
+    averageRating: number;
+    totalReviewCount: number;
+  };
+  productDescription: {
+    images: string[];
+  };
+  productDetailInfo: {
+    announcements: {
+      displayOrder: number;
+      name: string;
+      value: string;
+    }[];
+    terms: {
+      displayOrder: number;
+      title: string;
+      description: string;
+    }[];
+  };
+};
+
+export type ProductOptions = {
+  id: number;
+  name: string;
+  options: {
+    id: number;
+    name: string;
+    additionalPrice: number;
+  }[];
+};
+
+export type GoodsData = {
+  id: number;
+  name: string;
+  price: {
+    sellingPrice: number;
+  };
+  imageURL: string;
+  brandInfo: {
+    name: string;
+  };
+};
+
+export type RankingFilterOption = {
+  targetType: 'ALL' | 'FEMALE' | 'MALE' | 'TEEN';
+  rankType: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
+};
+
+export type ThemeData = {
+  id: number;
+  key: string;
+  label: string;
+  title: string;
+  description?: string;
+  backgroundColor: string;
+  imageURL: string;
 };
