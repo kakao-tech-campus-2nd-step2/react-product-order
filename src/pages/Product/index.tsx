005f3605dff51bff94cb179308d5071b5ab3c95b
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-19 14:10:24 +0900

    feat: add ProductInfo component

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Layout from '@components/features/Layout';
 import { Link } from 'react-router-dom';
-import { Image, Input, Button, useNumberInput, HStack } from '@chakra-ui/react';
-import { CenteredContainer, Container } from '@components/common';
-import ProductImage from '@assets/images/goodsItem.jpg';
-
-const IMAGE_SIZE = 450;
+import { Input, Button, useNumberInput, HStack } from '@chakra-ui/react';
+import { CenteredContainer } from '@components/common';
+import ProductInfo from '@components/features/Product/ProductInfo';
 
 export default function Product() {
   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } = useNumberInput({
@@ -23,22 +21,7 @@ export default function Product() {
     <Layout>
       <CenteredContainer maxWidth="lg">
         <InnerContainer>
-          <ProductContainer>
-            <article>
-              <Container justifyContent="space-between">
-                <Image src={ProductImage} maxW={IMAGE_SIZE} maxH={IMAGE_SIZE} mr={6} />
-                <div>
-                  <ProductTitle>[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)</ProductTitle>
-                  <ProductPrice>145000원</ProductPrice>
-                  <GiftInfo>
-                    <hr />
-                    <p>카톡 친구가 아니어도 선물 코드로 선물 할 수 있어요!</p>
-                    <hr />
-                  </GiftInfo>
-                </div>
-              </Container>
-            </article>
-          </ProductContainer>
+          <ProductInfo />
           <ProductOrderContainer>
             <QuantitySelector>
               <Title>[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)</Title>
@@ -71,34 +54,6 @@ const InnerContainer = styled.div`
   height: 100vh;
   justify-content: space-between;
   padding-top: 100px;
-`;
-
-const ProductContainer = styled.main`
-  max-width: 748px;
-`;
-
-const ProductTitle = styled.h2`
-  font-size: 24px;
-  padding-top: 24px;
-`;
-
-const ProductPrice = styled.p`
-  font-size: 30px;
-  padding-top: 16px;
-`;
-
-const GiftInfo = styled.div`
-  padding-top: 48px;
-  font-size: 14px;
-  font-weight: 700;
-
-  hr:first-child {
-    margin-bottom: 14px;
-  }
-
-  hr:last-child {
-    margin-top: 14px;
-  }
 `;
 
 const ProductOrderContainer = styled.aside`
