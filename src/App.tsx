718982fd6eabf9c0e0f595b62d22d8e909f1c321
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-07-21 01:46:09 +0900

    fix : rebase 잘못한거 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { AuthProvider } from './provider/Auth';
 import { Routes } from './routes';
 
 const queryClient = new QueryClient();
 
-const queryClient = new QueryClient();
-
 const App = () => {
   return (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </QueryClientProvider>
-    </ChakraProvider>
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
