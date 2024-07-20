9b6fe5cf4b41a53c188f40ef5a7c61f4e3ede546
JaeAnHan <jaean1999@naver.com>
2024-07-20 17:12:39 +0900

    feat(OrderLetter): 기존 Textarea의 onChange에 dispatchState를 전달해 값을 위로 보내던 구조 대신 react-hook-form의 register를 사용해 값을 확인하고 해당 컴포넌트에서 바로 토스트 메세지 띄우도록 수정

--- src/components/features/Order/atoms/OrderLetter.tsx
+++ src/components/features/Order/atoms/OrderLetter.tsx
@@ -1,26 +1,39 @@
-import { Box, Textarea } from '@chakra-ui/react';
-import { memo } from 'react';
+import { Box, Textarea, useToast } from '@chakra-ui/react';
+import { memo, useEffect } from 'react';
+import type { UseFormRegister } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 
+import type { OrderFormData } from '@/hooks/useOrderValidation';
 import { orderLetterPlaceHolder } from '@/pages/Order';
 
 export interface IOrderLetter {
-  setMessage: (message: string) => void;
+  register: UseFormRegister<OrderFormData>;
 }
 
-export const OrderLetter = memo(({ setMessage }: IOrderLetter) => {
-  const handleMessageChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-    setMessage(event.target.value);
-  };
+export const OrderLetter = memo(({ register }: IOrderLetter) => {
+  const { formState } = useFormContext<OrderFormData>();
+  const toast = useToast();
+
+  useEffect(() => {
+    if (formState.errors.message) {
+      toast({
+        title: formState.errors.message.message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'bottom',
+      });
+    }
+  }, [formState.errors.message, toast]);
 
   return (
     <Box width="100%" padding="26px 30px 30px">
       <Textarea
-        onChange={handleMessageChange}
         placeholder={orderLetterPlaceHolder}
-        resize="none"
-        height="100px"
-        variant="filled"
-        colorScheme="gray"
+        {...register('message', {
+          required: '카드 메세지를 입력해 주세요.',
+          maxLength: { value: 100, message: '카드 메세지를 100글자 이내로 입력해 주세요.' },
+        })}
       />
     </Box>
   );
