2ee4d23dbe44937477128a1e7a07e600f5d7097a
JeongSeoyun <xeonj@naver.com>
2024-07-20 21:55:19 +0900

    fix: 주문 작성시 예외처리 해결 및 타입 안전하도록 피드백 반영하여 코드 수정

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -12,9 +12,17 @@ import {
   Textarea,
   VStack,
 } from '@chakra-ui/react';
-import type { FieldError } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form'; // react-hook-form import 추가
 import { useLocation } from 'react-router-dom';
+
+interface FormValues {
+  message: string;
+  receiptNumber?: string;
+  receiptRequested: boolean;
+  receiptType?: string;
+}
+
 
 export const Order = () => {
   const location = useLocation();
@@ -25,27 +33,20 @@ export const Order = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm(); // useForm 훅을 사용하여 폼 상태와 메소드 초기화
+  } = useForm<FormValues>();
 
-  // ref 대신 register 함수 사용
-  const messageRef = register('message', {
-    required: '카드 메시지를 입력해주세요.',
-    maxLength: { value: 100, message: '카드 메시지는 100자 이내로 입력해주세요.' },
-  });
-  const receiptNumberRef = register('receiptNumber', {
-    required: '현금영수증 번호를 입력해주세요.',
-    pattern: { value: /^\d+$/, message: '현금영수증 번호는 숫자만 입력해주세요.' },
-  });
+  
+  const receiptRequested = watch('receiptRequested')
 
-  const onSubmit = (data: Record<string, null>) => {
-    if (data.message && !watch('receiptRequested')) {
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    if (data.message && !data.receiptRequested) {
       window.alert('주문이 완료되었습니다.');
-    } else if (data.message && watch('receiptRequested') && !errors.receiptNumber) {
+    } else if (data.message && data.receiptRequested && data.receiptNumber) {
       window.alert('주문이 완료되었습니다.');
     }
 
     console.log('Order Data:', data);
-    console.log(data.message, watch('receiptRequested'), !errors.reciptNumber);
   };
 
   return (
@@ -56,15 +57,18 @@ export const Order = () => {
             <Text fontSize="xl" mb={4} align={'center'} fontWeight={700}>
               나에게 주는 선물
             </Text>
-            {/* register 함수로 Input 요소를 등록 */}
+
             <Textarea
               placeholder="선물과 함께 보낼 메시지를 적어보세요"
               size="lg"
               mb={3}
               height={100}
-              {...messageRef}
+              {...register('message', {
+                required: '카드 메세지를 입력해주세요.',
+                maxLength: { value: 100, message: '카드 메시지는 100자 이내로 입력해주세요.' },
+              })}
             />
-            {errors.message && <Text color={'red'}>{(errors.message as FieldError)?.message}</Text>}
+            {errors.message && <Text color={'red'}>{errors.message?.message}</Text>}
 
             <Text fontSize="xl" mb={4} mt={10}>
               선물내역
@@ -91,15 +95,21 @@ export const Order = () => {
               <Checkbox mt={5} {...register('receiptRequested')}>
                 현금영수증 신청
               </Checkbox>
-              {watch('receiptRequested') && ( // 현금영수증 신청 시에만 출력
+              {receiptRequested && ( // 현금영수증 신청 시에만 출력
                 <>
                   <Select {...register('receiptType')}>
                     <option value="개인소득공제">개인소득공제</option>
                     <option value="사업자증빙용">사업자증빙용</option>
                   </Select>
-                  <Input placeholder="(- 없이) 숫자만 입력해주세요." {...receiptNumberRef} />
+                  <Input
+                    placeholder="(- 없이) 숫자만 입력해주세요."
+                    {...register('receiptNumber', {
+                      required: receiptRequested ? '현금영수증 번호를 입력해주세요.' : false,
+                      pattern: { value: /^\d+$/, message: '현금영수증 번호는 숫자만 입력해주세요.' }
+                    })}
+                  />
                   {errors.receiptNumber && (
-                    <Text color={'red'}>{(errors.receiptNumber as FieldError)?.message}</Text>
+                    <Text color={'red'}>{errors.receiptNumber?.message}</Text>
                   )}
                 </>
               )}
