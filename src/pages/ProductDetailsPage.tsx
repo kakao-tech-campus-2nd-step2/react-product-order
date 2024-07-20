f204d1a6e7cb46d13dfdb9ceb9368b446a771531
cla6shade <cla6shade@gmail.com>
2024-07-21 02:06:30 +0900

    refactor: product Id가 children에서 typesafe하도록 변경

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,13 +1,25 @@
-import { useParams } from 'react-router-dom';
-import { Suspense } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
 import Page from '@components/templates/Page';
 import Container from '@components/atoms/container/Container';
 import ProductDetailDisplaySection from '@components/organisms/product/ProductDetailDisplaySection';
 import { LoadingSpinnerFullWidth } from '@components/atoms/LoadingSpinner';
 import ErrorBoundary from '@components/atoms/boundary/ErrorBoundary';
+import Paths from '@constants/Paths';
 
 function ProductDetailsPage() {
   const { productId } = useParams<string>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!productId) {
+      navigate(Paths.MAIN_PAGE);
+    }
+  }, [productId, navigate]);
+
+  if (!productId) {
+    return null;
+  }
 
   return (
     <Page>
