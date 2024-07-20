107ff0f3747c8aeca4080e14ed1f27c58a88f270
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-20 22:05:32 +0900

    feat: improve form accessibility by handling form submit event

--- src/components/features/Order/Payment/ReceiptForm/index.tsx
+++ src/components/features/Order/Payment/ReceiptForm/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import { Checkbox, Select, Input } from '@chakra-ui/react';
+import { Button } from '@components/common';
 import { useFormContext } from 'react-hook-form';
+import { OrderDataFormValues } from '@/pages/Order';
+import { validatePayment } from './validation';
 
 export default function ReceiptForm() {
-  const { register, watch } = useFormContext();
+  const { register, watch, handleSubmit } = useFormContext<OrderDataFormValues>();
   const { hasCashReceipt } = watch();
 
+  const onSubmit = (data: OrderDataFormValues) => {
+    const errorMessage = validatePayment(data.message, data.hasCashReceipt, data.cashReceiptNumber);
+    if (errorMessage) return alert(errorMessage);
+    return alert('주문이 완료되었습니다.');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Checkbox mb={4} fontWeight={700} {...register('hasCashReceipt')}>
         현금영수증 신청
       </Checkbox>
@@ -20,6 +30,37 @@ export default function ReceiptForm() {
           <Input placeholder="(-없이) 숫자만 입력해주세요." mb={4} {...register('cashReceiptNumber')} />
         </>
       )}
+      <TotalAmount>
+        <dl>
+          <dt>최종 결제금액</dt>
+          <dd>49900원</dd>
+        </dl>
+      </TotalAmount>
+      <Button theme="kakao" type="submit">
+        49900원 결제하기
+      </Button>
     </form>
   );
 }
+
+const TotalAmount = styled.div`
+  padding: 18px 20px;
+  border-radius: 4px;
+  background-color: rgb(245, 245, 245);
+  margin-bottom: 20px;
+
+  dl {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: 700;
+  }
+
+  dt {
+    font-size: 14px;
+  }
+
+  dd {
+    font-size: 20px;
+  }
+`;
--- src/components/features/Order/Payment/index.tsx
+++ src/components/features/Order/Payment/index.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { useFormContext } from 'react-hook-form';
-import { Button } from '@components/common';
 import ReceiptForm from './ReceiptForm';
-import { validatePayment } from './validation';
 
 export default function Payment() {
-  const { watch } = useFormContext();
-  const { message, hasCashReceipt, cashReceiptNumber } = watch();
-
-  const handleOrderClick = () => {
-    const errorMessage = validatePayment(message, hasCashReceipt, cashReceiptNumber);
-    if (errorMessage) return alert(errorMessage);
-    return alert('주문이 완료되었습니다.');
-  };
-
   return (
     <PaymentContainer>
       <Title>결제정보</Title>
       <ReceiptForm />
-      <TotalAmount>
-        <dl>
-          <dt>최종 결제금액</dt>
-          <dd>49900원</dd>
-        </dl>
-      </TotalAmount>
-      <Button theme="kakao" onClick={handleOrderClick}>
-        49900원 결제하기
-      </Button>
     </PaymentContainer>
   );
 }
@@ -41,26 +20,4 @@ const Title = styled.span`
   font-size: 18px;
   font-weight: 700;
   margin-bottom: 12px;
-`;
-
-const TotalAmount = styled.div`
-  padding: 18px 20px;
-  border-radius: 4px;
-  background-color: rgb(245, 245, 245);
-  margin-bottom: 20px;
-
-  dl {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-weight: 700;
-  }
-
-  dt {
-    font-size: 14px;
-  }
-
-  dd {
-    font-size: 20px;
-  }
 `;
