090fe52e83ba0f70c3e9557e37fa930cea59aa4d
Eric <real.dassy@gmail.com>
2024-07-21 03:18:31 +0900

    feat: OrderPage 하위 컴포넌트 useFormContext 추가

--- src/components/features/Order/PaymentDetails.tsx
+++ src/components/features/Order/PaymentDetails.tsx
@@ -1,66 +1,53 @@
 import { Checkbox, HStack, Input, Select, Spacer, Text, VStack } from '@chakra-ui/react';
+import { useFormContext } from 'react-hook-form';
 
 import { CTAButton } from '@/components/common/Button/CTAButton';
+import type { OrderFormData } from '@/pages/Order/OrderPage';
 
-interface PaymentDetailsProps {
+interface Props {
   totalPrice: number;
-  isReceiptChecked: boolean;
-  setIsReceiptChecked: (value: boolean) => void;
-  receiptNumber: string;
-  setReceiptNumber: (value: string) => void;
-  handleClick: () => void;
 }
 
-export const PaymentDetails = ({
-  totalPrice,
-  isReceiptChecked,
-  setIsReceiptChecked,
-  receiptNumber,
-  setReceiptNumber,
-  handleClick,
-}: PaymentDetailsProps) => (
-  <VStack w="326px" h="100%" borderLeft="1px" borderColor="gray.200" p="16px">
-    <Text fontSize="18px" fontWeight="bold" pb="30px">
-      결제 정보
-    </Text>
+export const PaymentDetails = ({ totalPrice }: Props) => {
+  const { register } = useFormContext<OrderFormData>();
 
-    <VStack w="100%" borderY="1px" borderColor="gray.200">
-      <Checkbox
-        w="100%"
-        pt="12px"
-        alignItems="center"
-        isChecked={isReceiptChecked}
-        onChange={(e) => setIsReceiptChecked(e.target.checked)}
-      >
-        현금영수증 신청
-      </Checkbox>
-      <Select w="100%" placeholder="개인소득공제">
-        <option value="individual">사업자증빙용</option>
-      </Select>
-      <Input
-        w="100%"
-        type="text"
-        placeholder="(-없이) 숫자만 입력해주세요."
-        value={receiptNumber}
-        onChange={(e) => setReceiptNumber(e.target.value)}
+  return (
+    <VStack w="326px" h="100%" borderLeft="1px" borderColor="gray.200" p="16px">
+      <Text fontSize="18px" fontWeight="bold" pb="30px">
+        결제 정보
+      </Text>
+
+      <VStack w="100%" borderY="1px" borderColor="gray.200">
+        <Checkbox w="100%" pt="12px" alignItems="center" {...register('checkedReceipt')}>
+          현금영수증 신청
+        </Checkbox>
+        <Select w="100%" placeholder="개인소득공제">
+          <option value="individual">사업자증빙용</option>
+        </Select>
+        <Input
+          w="100%"
+          type="text"
+          placeholder="(-없이) 숫자만 입력해주세요."
+          {...register('receiptNumber')}
+        />
+      </VStack>
+
+      <HStack w="100%" borderY="1px" borderColor="gray.200">
+        <Text p="16px" fontSize="15px" fontWeight="bold">
+          최종 결제금액
+        </Text>
+        <Spacer />
+        <Text fontSize="18px" fontWeight="bold">
+          {totalPrice} 원
+        </Text>
+      </HStack>
+
+      <CTAButton
+        type="submit"
+        text={`${totalPrice}원 결제하기`}
+        textColor="black"
+        background="yellow"
       />
     </VStack>
-
-    <HStack w="100%" borderY="1px" borderColor="gray.200">
-      <Text p="16px" fontSize="15px" fontWeight="bold">
-        최종 결제금액
-      </Text>
-      <Spacer />
-      <Text fontSize="18px" fontWeight="bold">
-        {totalPrice} 원
-      </Text>
-    </HStack>
-
-    <CTAButton
-      onClick={handleClick}
-      text={`${totalPrice}원 결제하기`}
-      textColor="black"
-      background="yellow"
-    />
-  </VStack>
-);
+  );
+};
