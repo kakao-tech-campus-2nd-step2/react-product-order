ca78486ee2133d8900a6c326595b17357171f6eb
Eric <real.dassy@gmail.com>
2024-07-21 03:16:40 +0900

    feat: OrderPage FormProvider 추가

--- src/pages/Order/OrderPage.tsx
+++ src/pages/Order/OrderPage.tsx
@@ -1,10 +1,18 @@
 import { Flex, HStack, VStack } from '@chakra-ui/react';
+import type { SubmitHandler } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 
 import { useOrderForm } from '@/api/hooks/useOrderFoam';
 import { GiftDetails } from '@/components/features/Order/GiftDetails';
 import { OrderForm } from '@/components/features/Order/OrderFoam';
 import { PaymentDetails } from '@/components/features/Order/PaymentDetails';
+
+export interface OrderFormData {
+  message: string;
+  checkedReceipt: boolean;
+  receiptNumber: string;
+}
 
 export const OrderPage = () => {
   const location = useLocation();
@@ -20,21 +28,32 @@ export const OrderPage = () => {
     handleClick,
   } = useOrderForm();
 
+  const onSubmit: SubmitHandler<OrderFormData> = (data) => {
+    const errorMessage = validateOrder(data);
+
+    if (errorMessage) {
+      alert(errorMessage);
+    } else {
+      alert('주문이 정상적으로 처리 되었습니다.');
+    }
+  };
+
   return (
     <Flex minHeight="100vh" alignItems="top" justifyContent="center" padding="4">
-      <HStack borderLeft="1px" borderRight="1px" borderColor="gray.200">
-        <VStack px="60px" w="899px" h="100%">
-          <OrderForm message={message} setMessage={setMessage} />
-          <GiftDetails imageURL={imageURL} name={name} brandName={brandName} />
-        </VStack>
-        <PaymentDetails
-          totalPrice={totalPrice}
-          isReceiptChecked={isReceiptChecked}
-          setIsReceiptChecked={setIsReceiptChecked}
-          receiptNumber={receiptNumber}
-          setReceiptNumber={setReceiptNumber}
-          handleClick={handleClick}
-        />
+      <HStack
+        as="form"
+        onSubmit={methods.handleSubmit(onSubmit)}
+        borderLeft="1px"
+        borderRight="1px"
+        borderColor="gray.200"
+      >
+        <FormProvider {...methods}>
+          <VStack px="60px" w="899px" h="100%">
+            <OrderForm />
+            <GiftDetails imageURL={imageURL} name={name} brandName={brandName} />
+          </VStack>
+          <PaymentDetails totalPrice={totalPrice} />
+        </FormProvider>
       </HStack>
     </Flex>
   );
