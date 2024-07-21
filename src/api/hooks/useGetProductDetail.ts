4cb23cb8443eecfa31c8ba27f0337d7e87158f37
minji2219 <200146@jnu.ac.kr>
2024-07-21 22:34:37 +0900

    Feat: api hook 수정
    usequery select를 이용하여 코드 작성

--- src/api/hooks/useGetProductDetail.ts
+++ src/api/hooks/useGetProductDetail.ts
@@ -8,12 +8,12 @@ const getProductDeatilPath = (productId: string) => {
   return `/v1/products/${productId}/detail`;
 };
 export const getDetail = async (productId: string) => {
-  const response = await fetchInstance.get<ProductDetail>(getProductDeatilPath(productId));
-  return response.data.detail;
+  return fetchInstance.get<ProductDetail>(getProductDeatilPath(productId));
 };
 export const useGetDetail = (productId: string) => {
   return useQuery({
     queryKey: ['products', productId],
     queryFn: () => getDetail(productId),
+    select: (response) => response.data.detail,
   });
 };
--- src/api/hooks/useGetProductOptions.tsx
+++ src/api/hooks/useGetProductOptions.tsx
@@ -6,12 +6,12 @@ const getProductOptionPath = (productId: string) => {
   return `/v1/products/${productId}/options`;
 };
 export const getOption = async (productId: string) => {
-  const response = await fetchInstance.get(getProductOptionPath(productId));
-  return response.data.options;
+  return fetchInstance.get(getProductOptionPath(productId));
 };
 export const useGetOption = (productId: string) => {
   return useQuery({
     queryKey: ['option', productId],
     queryFn: () => getOption(productId),
+    select: (response) => response.data.options,
   });
 };
